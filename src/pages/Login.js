a3ce395b7eb764a8ff0fa4e30ff16026e8471ffb
Ediberto Oliveira <edibertooliveira@aol.com>
2021-03-02 18:47:28 -0300

    Finished Req 1 [screen login]

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
+// import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+import logoImage from '../trivia.png';
 
-class Login extends React.Component {
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      nickname: '',
+      isValidated: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.validateFields = this.validateFields.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState((state) => ({
+      ...state, [name]: value,
+    }), () => this.validateFields());
+  }
+
+  validateFields() {
+    const { email, nickname } = this.state;
+    const regex = new RegExp('[a-zA-Z0-9.]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+$', 'gm');
+    const nicknameLengthMin = 1;
+    const checkedEmail = regex.test(email);
+    const checkedNickname = nickname.length >= nicknameLengthMin;
+
+    if (checkedEmail && checkedNickname) {
+      return this.setState((state) => ({
+        ...state,
+        isValidated: false,
+      }));
+    }
+    this.setState((state) => ({
+      ...state,
+      isValidated: true,
+    }));
+  }
+
   render() {
+    const { email, nickname, isValidated } = this.state;
+    const { isGameStarted } = this.props;
+    if (isGameStarted) return <Redirect to="/game" />;
     return (
-      <div>Login</div>
+      <div className="loginContainer">
+        <img src={ logoImage } alt="logo" width="150px" />
+        <input
+          type="text"
+          data-testid="input-player-name"
+          name="nickname"
+          id="id-nickname"
+          value={ nickname }
+          placeholder="nick"
+          onChange={ this.handleChange }
+        />
+        <input
+          type="text"
+          data-testid="input-gravatar-email"
+          name="email"
+          id="id-email"
+          value={ email }
+          placeholder="email"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ !!isValidated }
+        >
+          Jogar
+        </button>
+      </div>
     );
   }
 }
 
-export default Login;
+// const mapStateToProps = (state) => ({
+//   isGameStarted: state,
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+//   startTheGame: bindActionCreators(null, dispatch),
+// });
+
+Login.propTypes = {
+  isGameStarted: PropTypes.bool,
+};
+
+Login.defaultProps = {
+  isGameStarted: false,
+};
+
+export default connect(null, null)(Login);
