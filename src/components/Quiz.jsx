60f2b260d5af4785d7adc5a0cd821a2b0c33748c
afonso <afonsoafn@gmail.com>
2021-03-02 16:42:52 -0300

    Arquitetura inicial

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Quiz extends Component {
+  render() {
+    return (
+      <div>
+        Eu sou o Quiz.
+      </div>
+    )
+  }
+}
