8c2ccd1f159344eddac1ec7709633d0b7a5aa829
Fabio Ferreira <fabio35547@gmail.com>
2021-03-02 16:29:48 -0300

    Criacao da esturtura padrao Redux e Router

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        Página de login
+      </div>
+    );
+  }
+}
+
+export default Login;
