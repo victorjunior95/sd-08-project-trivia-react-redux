976b30b4a60587a00a85a5b4424242d6a49b71e5
afonso <afonsoafn@gmail.com>
2021-03-02 19:01:44 -0300

    Arquitetura do header, quiz e da page game

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imagePatch: '',
+      playerName: '',
+      score: '',
+    };
+  }
+
   render() {
+    const { playerName, score, imagePatch } = this.state
     return (
-      <div>
-        Eu sou o Header.
-      </div>
+      <header>
+        <img src={imagePatch} />
+        <div>{playerName}</div>
+        <div>{score}</div>
+      </header>
     )
   }
 }
