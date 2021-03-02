6cf13f53d536129291264539d9426435ef35c463
vivianeflowt <vivianeflowt@gmail.com>
2021-03-02 18:21:34 -0300

    ok

---
+++ debug.js
@@ -0,0 +1,9 @@
+// const fetch = require('node-fetch');
+
+// const debug = async () => {
+//   const token = await requestToken();
+//   const res = await requestTriviaQuestions(token, 2);
+//   console.log(res);
+// };
+
+// debug();
