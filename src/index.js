6c96c191c8b41cf90097ba7f93e44cd53a174f68
adrianWarrior <adrianoforcellini>
2021-03-02 16:37:01 -0300

    solving conflicts

--- src/index.js
+++ src/index.js
@@ -15,5 +15,5 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root'),
 );
-
+...
 serviceWorker.unregister();
---
+++ src/pages/Game.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  render() {
+    return (
+      <div>
+        games
+      </div>
+    );
+  }
+}
+
+export default Game;
