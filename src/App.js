e8b1478600ef81a7588a940d15419453113cb8c3
Paulo Simões <paulohbsimoes@gmail.com>
2021-03-02 15:39:41 -0300

    Estruturando pastas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import logo from './trivia.png';
-import './App.css';
+
+import './styles/index.css';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>
           SUA VEZ
         </p>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import styles from '../styles/pages/Home.module.css';
+
+class Home extends Component {
+  render() { 
+    return (
+      <h1>Home</h1>
+    );
+  }
+}
+ 
+export default Home;
