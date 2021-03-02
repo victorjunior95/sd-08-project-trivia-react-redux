0b4c58b51daa1b971eb1f139faf9980cbb3190a4
adrianWarrior <adrianoforcellini>
2021-03-02 16:21:48 -0300

    Requisito 1 pronto

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
   render() {
@@ -30,6 +31,9 @@ class Login extends Component {
             >
               Play
             </button>
+            <Link to="./settings">
+              <div className="engrenagem" />
+            </Link>
           </div>
         </main>
       </div>
