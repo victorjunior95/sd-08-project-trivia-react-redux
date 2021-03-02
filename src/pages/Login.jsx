00daac418362281a1b48b7d54828ad9ff346d5cb
emassaki <massakihirayama@gmail.com>
2021-03-02 19:20:25 -0300

    Corrigindo erros e refatorando

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import getToken from '../services';
 import { saveUserData } from '../_redux/action';
@@ -12,8 +12,6 @@ class Login extends Component {
     this.state = {
       name: '',
       email: '',
-      goToGame: false,
-      goToConfig: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -24,16 +22,13 @@ class Login extends Component {
     this.setState({ [name]: value });
   }
 
-  async handleClick({ target }) {
+  async handleClick() {
     const { email, name } = this.state;
     const { saveUser } = this.props;
     saveUser({ email, name });
-    this.setState({ [target.name]: true });
-    if (target.name === 'goToGame') {
-      const triviaAPIResponse = await getToken();
-      const { token } = triviaAPIResponse;
-      localStorage.setItem('token', JSON.stringify(token));
-    }
+    const triviaAPIResponse = await getToken();
+    const { token } = triviaAPIResponse;
+    localStorage.setItem('token', JSON.stringify(token));
   }
 
   validator() {
@@ -44,9 +39,7 @@ class Login extends Component {
   }
 
   render() {
-    const { name, email, goToGame, goToConfig } = this.state;
-    if (goToGame) return <Redirect to="/trivia" />;
-    if (goToConfig) return <Redirect to="/config" />;
+    const { name, email } = this.state;
     return (
       <div>
         <input
@@ -65,23 +58,27 @@ class Login extends Component {
           placeholder="Email"
           onChange={ this.handleChange }
         />
-        <button
-          type="button"
-          data-testid="btn-play"
-          name="goToGame"
-          disabled={ !this.validator() }
-          onClick={ this.handleClick }
-        >
-          Jogar
-        </button>
-        <button
-          type="button"
-          data-testid="btn-settings"
-          name="goToConfig"
-          onClick={ this.handleClick }
-        >
-          Config
-        </button>
+        <Link to="/trivia">
+          <button
+            type="button"
+            data-testid="btn-play"
+            name="goToGame"
+            disabled={ !this.validator() }
+            onClick={ this.handleClick }
+          >
+            Jogar
+          </button>
+        </Link>
+        <Link to="/config">
+          <button
+            type="button"
+            data-testid="btn-settings"
+            name="goToConfig"
+            onClick={ this.handleClick }
+          >
+            Config
+          </button>
+        </Link>
       </div>
     );
   }
