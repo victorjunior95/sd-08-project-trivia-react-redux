76e1207834039e10b9102413290d06e074048e09
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-03-02 20:43:48 -0300

    Corrigidos erros no App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import logo from './trivia.png';
 import './App.css';
-import { Provider } from 'react-redux';
 
 export default function App() {
   return (
-    <Provider store={store}>
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
+    <Provider store={ store }>
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>
+            SUA VEZ
+          </p>
+        </header>
+      </div>
     </Provider>
   );
 }
