e39be5fb1df98c0ad3b3a4d2b8d0dd79660b1199
Rosiele David <rosieleserpa@hotmail.com>
2021-03-02 17:27:32 -0300

    cria pagina de configuraçoes e ranking

---
+++ src/pages/settings/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SettingsForm from '../../components/SettingsForm';
+
+export default class Settings extends React.Component {
+  render() {
+    return (
+      <SettingsForm />
+    );
+  }
+}
