cf9b5cac381b7aa54130426de21fab550bda2722
VitorLimaRios <vitorlimarios999@gmail.com>
2021-03-02 16:10:42 -0300

    adicionando Router e pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import logo from './trivia.png';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Game from './pages/Game';
+import Feedback from './pages/Feedback';
+import Rank from './pages/Rank';
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/rank" component={ Rank } />
+      <Route path="/feedback" component={ Feedback } />
+      <Route path="/game" component={ Game } />
+      <Route path="/" component={ Login } />
+    </Switch>
   );
 }
---
+++ src/pages/Feedback.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Feedback extends React.Component {
+  render() {
+    return <div>INICIO</div>;
+  }
+}
+
+export default Feedback;
