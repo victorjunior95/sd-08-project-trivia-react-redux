6c7cd4c8d1ab543a94dd033601f3f9619f884296
Douglas Ferreira <dodofp.df@gmail.com>
2021-03-02 19:21:42 -0300

    MAIN-GROUP-30-REQUISITO-3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import { Login, Game } from './pages/index';
+import { Login, Game, Settings } from './pages/index';
 
 export default function App() {
   return (
@@ -10,6 +10,7 @@ export default function App() {
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route path="/game" component={ Game } />
+          <Route path="/settings" component={ Settings } />
         </Switch>
       </header>
     </div>
