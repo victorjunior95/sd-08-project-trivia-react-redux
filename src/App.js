b14a2d201393044efbea8823b60d75adc7370bd4
Mateus Coury <mateuscoury@gmail.com>
2021-03-02 15:12:59 -0300

    primeiro merge

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SUA VEZ
+          SUA VEZZ
         </p>
       </header>
     </div>
