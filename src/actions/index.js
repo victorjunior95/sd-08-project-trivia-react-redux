42773545a3d1f0689a2b3ce5eeea5268fcfbe4ca
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-03-02 16:56:20 -0300

    Cria login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,10 @@
 export const LOGIN = 'LOGIN';
+export const INPUT_VALUE = 'INPUT_VALUE'
+
+export const saveInputs = (payload) => ({
+  type: INPUT_VALUE,
+  payload,
+});
 
 export const loginAction = (payload) => ({
   type: LOGIN,
