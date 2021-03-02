2fa419361477c7bef5bb45a03209f867f4f421d9
Thais Quintela <thais.s.quintela@gmail.com>
2021-03-02 17:59:51 -0300

    Cria a pagina de feedback

--- src/pages/feedback/index.js
+++ src/pages/feedback/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 
 export default class index extends Component {
-    render() {
-        return (
-            <div>
-                Eu sou o feedback.
-            </div>
-        )
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      totalQuestions,
+      score,
     }
-}
+  }
+
+  render() {
+    const { totalQuestions, score } = this.props;
+    return (
+      <section>
+        <h1 data-testid="feedback-text">
+          { totalQuestions >= 3 ? 'Mandou bem!' : 'Podia ser melhor...' }
+        </h1>
+        <p data-testid="feedback-total-question">Você acertou { totalQuestions } questões!</p>
+        <p data-testid="feedback-total-score">Um total de { score } pontos</p>
+        <button data-testid="btn-ranking">Ver Ranking</button>
+        <button data-testid="btn-play-again">Jogar novamente</button>
+      </section>
+    )
+  };
+};
