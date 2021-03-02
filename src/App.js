19bb2d58ef6410b4c64897e9acd7898361b5d6bd
Rita Jeveaux <ritajeveaux@gmail.com>
2021-03-02 15:49:20 -0300

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import logo from './trivia.png';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
+import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
+import GamePage from './pages/GamePage';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/settings" component={ Settings } />
+        <Route path="/game" component={ GamePage } />
+        <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
+      </Switch>
     </div>
   );
 }
