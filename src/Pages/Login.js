c1dfd0b25b7a7d9d277e52b277d616b34e0f044e
Bidfca <bruninhakjurugat11@gmail.com>
2021-03-03 13:47:06 -0300

    main-group-32-req2

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,25 +1,33 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.newState = this.newState.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.state = {
-      name: '',
-      email: '',
+      fields: { name: '', email: '' },
+      shouldRedirect: false,
     };
   }
 
-  newState({ target }) {
+  handleInputChange({ target }) {
     const { name, value } = target;
-    this.setState({ [name]: value });
+    this.setState(({ fields }) => ({ fields: { ...fields, [name]: value } }));
+  }
+
+  checkValidity() {
+    const { fields: { name, email } } = this.state;
+    if (name === '' || email === '') {
+      return true;
+    }
+    return false;
   }
 
   render() {
-    const {
-      name,
-      email,
-    } = this.state;
+    const { fields: { name, email }, shouldRedirect } = this.state;
+
+    if (shouldRedirect) return (<Redirect to="play" />);
     return (
       <div>
         <form>
@@ -29,7 +37,7 @@ class Login extends React.Component {
             name="name"
             value={ name }
             data-testid="input-player-name"
-            onChange={ this.newState }
+            onChange={ this.handleInputChange }
           />
           <h3>Email</h3>
           <input
@@ -37,24 +45,16 @@ class Login extends React.Component {
             name="email"
             value={ email }
             data-testid="input-gravatar-email"
-            onChange={ this.newState }
+            onChange={ this.handleInputChange }
           />
-          {name === '' || email === ''
-            ? (
-              <button
-                type="button"
-                data-testid="btn-play"
-                disabled
-              >
-                Jogar
-              </button>)
-            : (
-              <button
-                type="button"
-                data-testid="btn-play"
-              >
-                Jogar
-              </button>)}
+          <button
+            type="button"
+            data-testid="btn-play"
+            disabled={ this.checkValidity() }
+            onClick={ () => this.setState({ shouldRedirect: true }) }
+          >
+            Jogar
+          </button>
         </form>
       </div>
     );
