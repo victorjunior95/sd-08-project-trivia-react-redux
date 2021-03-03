e6599b74c3302bce69abbb2106c9ff04c89b185a
josebejohann <josebernardojohann@gmail.com>
2021-03-03 17:04:40 -0300

    Implements a <Header /> component for the game page

---
+++ src/pages/Game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../components/Header';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+
+export default Game;
