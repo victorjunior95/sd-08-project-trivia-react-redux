6c7a52d518a1cb117745c479008483e21d899486
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-03-03 16:24:37 -0300

    REQ-1: Pagina de login criada

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import logo from './trivia.png';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <Provider store={ store }>
-      <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p>
-            SUA VEZ
-          </p>
-        </header>
-      </div>
-    </Provider>
+    <Switch>
+      <Route exact path="/" component={ Home } />
+    </Switch>
   );
 }
