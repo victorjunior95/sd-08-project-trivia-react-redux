3a5fc4d1acf1b21c7e70300273bc7dbf7e06e721
afonso <afonsoafn@gmail.com>
2021-03-03 15:52:53 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import logo from './trivia.png';
+// import logo from './trivia.png';
 import './App.css';
 import { feedback } from './pages'
+
+import { game } from './pages'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={feedback} />
+        <Route path="/" component={game} />
       </Switch>
       {/* <div className="App">
       <header className="App-header">
--- src/pages/feedback/index.js
+++ src/pages/feedback/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
 export default class index extends Component {
-    render() {
-        return (
-            <div>
-                Eu sou o feedback.
+  render() {
+    return (
+      <div>
+      Eu sou o feed
             </div>
         )
     }
