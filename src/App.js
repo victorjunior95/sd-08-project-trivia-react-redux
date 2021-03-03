88431240c0f11d3bc29bbd6376d798f392d2acd6
Blucilha <blucilha.nascimento@gmail.com>
2021-03-03 09:00:28 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import { Login, Play } from './pages';
+import { Login, Play, Settings } from './pages';
 
 export default function App() {
   return (
@@ -9,6 +9,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/game" component={ Play } />
+        <Route path="/settings" component={ Settings } />
       </Switch>
     </div>
   );
