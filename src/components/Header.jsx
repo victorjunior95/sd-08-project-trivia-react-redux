03009c2bf5e2859a3c92fe5b479eb11910cc5016
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-03-03 15:48:22 -0300

    Correçao Requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+/* import PropTypes from 'prop-types'; */
 import md5 from 'crypto-js/md5';
 
-export default function Header({ name, email, score }) {
+export default function Header() {
   const avatarFromEmail = (mail) => {
     const hash = md5(mail);
     const url = `https://www.gravatar.com/avatar/${hash}`;
     return url;
   };
-  /* const { user } = localStorage.setItem('state', JSON.parse({ player })); */
+  const { player } = JSON.parse(localStorage.getItem('state'));
 
-  const avatar = avatarFromEmail(email);
+  const avatar = avatarFromEmail(player.email);
   return (
     <div className="header">
       <h1>Tela Principal</h1>
@@ -21,18 +21,12 @@ export default function Header({ name, email, score }) {
       />
       <h3 data-testid="header-player-name">
         Nome:
-        {name}
+        {player.name}
       </h3>
       <h3 data-testid="header-score">
         Placar:
-        {score}
+        {player.score}
       </h3>
     </div>
   );
 }
-
-Header.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  score: PropTypes.number.isRequired,
-};
