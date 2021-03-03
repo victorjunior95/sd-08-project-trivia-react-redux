db8087a4aea59032e10c3e69e230617d62e44b42
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-03-03 16:29:34 -0300

    REQ-1: Erro de importaçao corrigido

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
