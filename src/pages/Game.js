76b4f14cb48c9bf3222f2a699e91929376790b14
VitorLimaRios <vitorlimarios999@gmail.com>
2021-03-03 16:08:50 -0300

    [Group-19][Req-4/5] - Adicionando logica para dar disable nas alternativas

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -8,11 +8,10 @@ import { fetchAPI } from '../redux/actions';
 import '../css/game.css';
 
 class Game extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      disabled: false,
       quantity: 5,
     };
 
@@ -20,13 +19,15 @@ class Game extends React.Component {
   }
 
   async componentDidMount() {
-    const { data, token } = this.props;
+    const { data } = this.props;
+    const token = localStorage.getItem('token');
     const { quantity } = this.state;
     data(quantity, token);
   }
 
   componentDidUpdate() {
-    const { data, token, questions } = this.props;
+    const { data, questions } = this.props;
+    const token = localStorage.getItem('token');
     const { quantity } = this.state;
     if (token && questions.length < 1) return data(quantity, token);
   }
@@ -34,7 +35,7 @@ class Game extends React.Component {
   selectAnswer(event) {
     event.target.classList.add('selected');
     const buttons = document.querySelectorAll('.answer');
-    buttons.map((item) => item.disabled = 'true');
+    buttons.forEach((item) => item.setAttribute('disabled', 'true'));
   }
 
   randomAnswer(correct, incorrects) {
@@ -44,7 +45,6 @@ class Game extends React.Component {
         data-testid="correct-answer"
         onClick={ this.selectAnswer }
         className="answer correct"
-        disabled={ this.disabled }
         key={ 3 }
       >
         {correct}
@@ -55,14 +55,13 @@ class Game extends React.Component {
         data-testid={ `wrong-answer-${index}` }
         onClick={ this.selectAnswer }
         className="answer wrong "
-        disabled={ this.disabled }
         key={ index }
       >
         {incorrect}
       </button>));
     const answersList = [correctAnswer, ...incorrectAnswers];
     const answersShuffled = arrayShuffle(answersList);
-    return (answersShuffled.map((answer) => answer));
+    return answersShuffled;
   }
 
   questionsGenerator() {
@@ -106,7 +105,6 @@ const mapStateToProps = (state) => ({
   email: state.login.email,
   questions: state.game.questions,
   resquesting: state.game.resquesting,
-  token: state.login.token,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -130,5 +128,4 @@ Game.propTypes = {
     },
   )).isRequired,
   data: PropTypes.func.isRequired,
-  token: PropTypes.string.isRequired,
 };
