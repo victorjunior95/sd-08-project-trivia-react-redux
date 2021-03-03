e6337f6b8e9a8e7950deae4c73c22cfec764a125
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-03-03 13:34:50 -0300

    conclui req 5

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,97 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import md5 from 'crypto-js/md5';
-import fetchTriviaQuest from '../services/requestTriviaQuest';
+import Quests from './Quests';
 
 class Game extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      quests: [],
-      questionOrder: 0,
-    };
-  }
-
-  componentDidMount() {
-    fetchTriviaQuest().then((quest) => {
-      this.setState({
-        quests: quest.results,
-      });
-    });
-  }
-
   render() {
     const { email, playerName } = this.props;
     const emailHash = md5(email).toString();
-    const { quests } = this.state;
-    console.log(quests);
-    if (quests.length > 0) {
-      return (
-        <>
-          <section className="game-header">
-            <div>
-              <img
-                data-testid="header-profile-picture"
-                src={ `https://www.gravatar.com/avatar/${emailHash}` }
-                alt="player-img"
-              />
-              Jogador:
-              <span data-testid="header-player-name">{ playerName }</span>
-            </div>
-            <div>
-              Pontos:
-              <span data-testid="header-score"> 0</span>
-            </div>
-          </section>
-          <section className="game-question">
-            <h2 data-testid="question-category">
-              Categoria:
-              { quests[0].category }
-            </h2>
-            <h2 data-testid="question-text">
-              { quests[0].question }
-            </h2>
-            <button
-              type="button"
-              data-testid="correct-answer"
-            >
-              { quests[0].correct_answer }
-            </button>
-            <button
-              type="button"
-              data-testid="wrong-answer-0"
-            >
-              { quests[0].incorrect_answers[0] }
-            </button>
-            <button
-              type="button"
-              data-testid="wrong-answer-1"
-            >
-              { quests[0].incorrect_answers[1] }
-            </button>
-            <button
-              type="button"
-              datta-testid="wrong-answer-2"
-            >
-              { quests[0].incorrect_answers[2] }
-            </button>
 
-            <button type="button">PRÓXIMA</button>
-          </section>
-        </>
-      );
-    }
-    return (<span>Loading</span>);
+    return (
+      <>
+        <section className="game-header">
+          <div>
+            <img
+              data-testid="header-profile-picture"
+              src={ `https://www.gravatar.com/avatar/${emailHash}` }
+              alt="player-img"
+            />
+            Jogador:
+            <span data-testid="header-player-name">{ playerName }</span>
+          </div>
+          <div>
+            Pontos:
+            <span data-testid="header-score"> 0</span>
+          </div>
+        </section>
+        <section className="game-question">
+          <Quests />
+        </section>
+      </>
+    );
   }
 }
 Game.propTypes = {
-  email: PropTypes.string.isRequired,
-  playerName: PropTypes.string.isRequired,
+  email: PropTypes.string,
+  playerName: PropTypes.string,
 };
-const mapStateToProps = ({ login: { email, playerName } }) => ({
-  email,
-  playerName,
+Game.defaultProps = {
+  email: '',
+  playerName: '',
+};
+const mapStateToProps = (state) => ({
+  email: state.login.email,
+  playerName: state.login.playerName,
 });
 // const mapDispatchToProps = (dispatch) => ({});
 
--- src/services/requestTriviaQuest.js
+++
@@ -1,5 +0,0 @@
-export default async function fetchTriviaQuest() {
-  const token = localStorage.getItem('token');
-  const questObj = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
-  return questObj.json();
-}
