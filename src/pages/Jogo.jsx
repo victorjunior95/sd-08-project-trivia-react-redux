e0c72bd29def9fe00acb38d37a338eebf971d062
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-03-03 16:44:28 -0300

    req4 ok

---
+++ src/pages/Jogo.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+
+class Jogo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fotoJogador: '',
+    };
+
+    this.verificaEmail = this.verificaEmail.bind(this);
+  }
+
+  componentDidMount() {
+    this.verificaEmail();
+  }
+
+  verificaEmail() {
+    const { email } = this.props;
+    const emailPasso1 = md5(email);
+    const converteEmail = emailPasso1.toString();
+    const request = `https://www.gravatar.com/avatar/${converteEmail}`;
+    console.log(request);
+    this.setState({
+      fotoJogador: request,
+    });
+  }
+
+  render() {
+    const { nome } = this.props;
+    const { fotoJogador } = this.state;
+    return (
+      <header>
+        <img
+          src={ fotoJogador }
+          alt="imagem perfil jogador"
+          data-testid="header-profile-picture"
+        />
+        <h3 data-testid="header-player-name">{ nome }</h3>
+        <div data-testid="header-score">Score: 0</div>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = ({ player }) => ({
+  nome: player.nome,
+  email: player.email,
+});
+
+export default connect(mapStateToProps)(Jogo);
+
+Jogo.propTypes = {
+  email: PropTypes.string.isRequired,
+  nome: PropTypes.string.isRequired,
+};
