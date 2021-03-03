283723e8e4cbcad38fb91dfe43558b40c496db2a
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-03-03 18:53:49 -0300

    criando botao proxima pergunta

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -5,14 +5,35 @@ import Header from './Header';
 import CardQuestion from './CardQuestion';
 
 class Game extends React.Component {
+  constructor() {
+    super();
+    this.handleCLick = this.handleCLick.bind(this);
+    this.state = {
+      questionIndex: 0,
+    };
+  }
+
+  handleCLick() {
+    const questionsSize = 4;
+    const { questionIndex } = this.state;
+    if (questionIndex < questionsSize) {
+      this.setState((previousState) => ({
+        questionIndex: previousState.questionIndex + 1,
+      }));
+    }
+  }
+
   render() {
+    const { questionIndex } = this.state;
     const { questions } = this.props;
     return (
       <div>
         <Header />
         <div>GAME</div>
-        <CardQuestion question={ questions[0] } />
-        <button type="button">Próximo</button>
+        <CardQuestion question={ questions[questionIndex] } />
+        <button type="button" data-testid="btn-next" onClick={ this.handleCLick }>
+          Próximo
+        </button>
       </div>
     );
   }
