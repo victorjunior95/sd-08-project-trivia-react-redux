3f6e50c93ae0346d3ca9779ed87c1e259b31ebca
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-03-03 16:13:57 -0300

    Correçao3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 /* import PropTypes from 'prop-types'; */
 import md5 from 'crypto-js/md5';
 
-export default function Header() {
-  const avatarFromEmail = (mail) => {
+export default class Header extends Component {
+  avatarFromEmail(mail) {
     const hash = md5(mail);
     const url = `https://www.gravatar.com/avatar/${hash}`;
     return url;
-  };
-  const { player = {} } = JSON.parse(localStorage.getItem('state'));
+  }
 
-  const avatar = avatarFromEmail(player.email);
-  return (
-    <div className="header">
-      <h1>Tela Principal</h1>
-      <img
-        data-testid="header-profile-picture"
-        src={ avatar }
-        alt="avatar"
-      />
-      <h3 data-testid="header-player-name">
-        Nome:
-        {player.name}
-      </h3>
-      <h3 data-testid="header-score">
-        Placar:
-        {player.score}
-      </h3>
-    </div>
-  );
+  render() {
+    const { player = {} } = JSON.parse(localStorage.getItem('state'));
+    const avatar = this.avatarFromEmail(player.email);
+    return (
+      <div className="header">
+        <h1>Tela Principal</h1>
+        <img
+          data-testid="header-profile-picture"
+          src={ avatar }
+          alt="avatar"
+        />
+        <h3 data-testid="header-player-name">
+          Nome:
+          {player.name}
+        </h3>
+        <h3 data-testid="header-score">
+          Placar:
+          {player.score}
+        </h3>
+      </div>
+    );
+  }
 }
