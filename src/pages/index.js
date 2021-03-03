e8ea42a776db27a9cfd7a46b850c1d7cd0aa3f5a
Bruno Martins <bruno.martins217@gmail.com>
2021-03-03 11:48:44 -0300

    :beetle: Requisito 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,4 @@
-export { default as Login } from './Login';
-export { default as Settings } from './Settings';
+import Login from './Login';
+import Settings from './Settings';
+
+export { Login, Settings };
