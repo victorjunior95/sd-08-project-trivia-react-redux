98a4eff1f4f79f56b1d13cfccd7ec174f6462765
LukeCruz <enio.cruz@yahoo.com.br>
2021-03-03 13:40:46 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Gamescreen from './pages/Gamescreen';
+import {Settings} from './pages/Settings';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/settings" component={ Settings } />
         <Route exact path="/gamescreen" component={ Gamescreen } />
       </Switch>
     );
