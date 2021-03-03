3169429aacbd08fcaa8f2426708157d746baabdd
gabrieldeori <deori.pessoal@gmail.com>
2021-03-03 13:10:54 -0300

    Adicionando actions do fetch de questões

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,24 @@
 export const SAVE_LOGIN = 'SAVE_LOGIN';
+export const FETCH_QUESTIONS = 'FETCH_QUESTIONS';
+export const SAVE_QUESTIONS = 'SAVE_QUESTIONS';
+export const FETCH_ERROR = 'FETCH_ERROR';
 
 export const saveUserLogin = (payload) => ({ type: SAVE_LOGIN, payload });
+
+export const saveQuestions = (payload) => ({ type: SAVE_QUESTIONS, payload });
+export const fetchError = (payload) => ({ type: FETCH_ERROR, payload });
+
+export function fetchQuestions(token) {
+  return async (dispatch) => {
+    try {
+      const url = `https://opentdb.com/api.php?amount=5&token=${token}`;
+      const response = await fetch(url);
+      const questions = await response.json();
+      const payload = { questions };
+      return dispatch(saveQuestions(payload));
+    } catch (error) {
+      const payload = { error };
+      return dispatch(fetchError(payload));
+    }
+  };
+}
