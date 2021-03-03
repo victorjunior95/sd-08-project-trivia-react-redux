e61c46178d3f2c46952ee8bc58f33e8bf5843559
Tandy <tandyfw@gmail.com>
2021-03-03 15:38:43 -0300

    Requisito 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,10 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 
+const MINIMUM_ASSERTIONS = 3;
+
 class Feedback extends React.Component {
+  feedback() {
+    const assertions = localStorage.getItem('assertions');
+    if (assertions < MINIMUM_ASSERTIONS) {
+      return <span data-testid="feedback-text">Podia ser melhor...</span>;
+    } if (assertions >= MINIMUM_ASSERTIONS) {
+      return <span data-testid="feedback-text">Mandou bem!</span>;
+    }
+  }
+
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        { this.feedback() }
+      </>
     );
   }
 }
