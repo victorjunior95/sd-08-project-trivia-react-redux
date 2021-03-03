fe7c2a8a82802535b36f3c10dd39214157b5fa2a
Vitor Nunes <vitornunes200@gmail.com>
2021-03-03 18:21:11 -0300

    Req17 - created ranked players list with local storage testing

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import RankList from '../components/RankList';
 
 class Ranking extends React.Component {
   render() {
     return (
       <div>
+        <RankList />
         <Link to="/">
           <button type="button" data-testid="btn-go-home">Voltar</button>
         </Link>
