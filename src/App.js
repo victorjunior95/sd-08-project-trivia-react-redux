44f4794371b6304e48e1cfd858be164b3f06203c
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-03-02 22:59:59 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Home from './pages/Home';
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return (
     <Switch>
-      <Router exact path='/' component={Home} />
+      <Route exact path="/" component={ Home } />
     </Switch>
   );
 }
