e0a343e2a4e4b0c3798076d6413a2c46dd16677f
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-03-03 10:55:25 -0300

    salva index

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,7 @@
 import md5 from 'crypto-js/md5';
 
 export const LOGIN = 'LOGIN';
+const GET_QUESTIONS = 'GET_QUESTIONS';
 
 export const login = (player, token) => ({
   type: LOGIN,
@@ -10,12 +11,49 @@ export const login = (player, token) => ({
   },
 });
 
+export const getQuestions =(questions) => ({
+  type: GET_QUESTIONS,
+  payload: {
+    questions,
+  }
+})
+
+export const game = ()
+
 export function requestToken(name, email) {
   const gravatarEmail = md5(email).toString();
   const player = { name, gravatarEmail };
   return async (dispatch) => {
-    await fetch('https://opentdb.com/api_token.php?command=request')
-      .then((response) => response.json())
-      .then((data) => dispatch(login(player, data.token)));
+    const token = localStorage.getItem('token');
+    if (token) dispatch(requestQuestions(token))
+
+    //   const response = requestQuestions(token);
+    //   response.response_code  === 3 
+    //   ? await fetch('https://opentdb.com/api_token.php?command=request')
+    //  .then((response) => response.json())
+    //  .then((data) => dispatch(login(player, data.token)))
+    //  : 
+    // }
+
+
+      
   };
 }
+
+export const requestNewToken = () => {
+    const newToken = await fetch('https://opentdb.com/api_token.php?command=request')
+     .then((response) => response.json())
+     .then((data) => data.token);
+}
+
+export async function requestQuestions(token) {
+  const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`)
+    .then((response) => response.json())
+    .then((data) => data);
+  return response.response_code === 0 
+  ? getQuestions(response.results)
+  : 
+  
+}
+
+
