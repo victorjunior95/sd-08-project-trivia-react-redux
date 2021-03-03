a0e9bfd0e7b7ccdf778ae8c66938220a15d6a631
Ana Karine <anakarine.ti@gmail.com>
2021-03-03 12:49:02 -0300

    header-feedback-questions

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Feedback extends Component {
   render() {
-    return <div>feedback</div>;
+    return (
+      <div>
+        <Header />
+      </div>
+    );
   }
 }
