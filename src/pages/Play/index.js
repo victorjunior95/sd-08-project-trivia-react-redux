0acc47ff8a03238efd27aaaac3944036c4789402
Marcela <marcela9908@gmail.com>
2021-03-03 18:24:37 -0300

    alteraçao branch

--- src/pages/Play/index.js
+++ src/pages/Play/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import getToken from '../../services/getToken';
+import getQuestions from '../../servicues/getQuestions';
 
 class Play extends React.Component {
+  componentDidMount() {
+    getToken().then((data) => getQuestions(5, data.token).then((data) => console.log(data)));
+  }
+
   render() {
     return (
       <div>
@@ -11,3 +17,46 @@ class Play extends React.Component {
 }
 
 export default Play;
+
+// ./Actions
+
+const requestQuestions = () => ({
+  type: 'REQUEST_QUESTION',
+});
+
+const receiveQuestions = (objectQuestion) => ({
+  type: 'RECEIVE_QUESTION',
+  objectQuestion,
+});
+
+
+export function fetchQuestions() {
+  return (dispatch) => {
+    dispatch(requestQuestions());
+    return getToken().then(({token}) => getQuestions(5, token).then((data) => dispatch(receiveQuestions(data)))
+  };
+}
+
+// ./Reducer/Questions
+
+const INITIAL_STATE = {
+  data,
+  isFetching: false
+};
+
+function Questions(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'REQUEST_QUESTION':
+    return { ...state, isFetching: true };
+  case 'RECEIVE_QUESTION':
+    return { ...state, data: action.objectQuestion, isFetching: false };
+  default:
+    return state;
+  }
+}
+
+export default Questions;
+
+
+
+.reducer/index
