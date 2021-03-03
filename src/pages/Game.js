525db5d764506d077c297606174c4985fed0c31a
Douglas Ferreira <dodofp.df@gmail.com>
2021-03-03 17:07:14 -0300

    Requisito 5 sem avaliador

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,19 +1,77 @@
 import React from 'react';
 import Header from '../components/Header';
-import { getToken } from '../services';
+import { getQuestions } from '../services';
 
 class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      questions: {},
+      index: 0,
+    };
+    this.getAPI = this.getAPI.bind(this);
+    this.renderQuestions = this.renderQuestions.bind(this);
+    this.handleNext = this.handleNext.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAPI();
+  }
+
+  async getAPI() {
+    const fetch = await getQuestions();
+    this.setState({
+      questions: fetch,
+    });
+  }
+
+  handleNext() {
+    const { index, questions } = this.state;
+    if (index !== questions.length) {
+      this.setState({
+        index: index + 1,
+      });
+    }
+  }
+
+  renderQuestions() {
+    const { questions, index } = this.state;
+    const incorrect = questions[index].incorrect_answers;
+    console.log(incorrect);
+    return (
+      <div>
+        <p data-testid="question-category">{questions[index].category}</p>
+        <h5 data-testid="question-text">{questions[index].question}</h5>
+        <section>
+          <button
+            data-testid="correct-answer"
+            type="button"
+          >
+            {questions[index].correct_answer}
+          </button>
+          {incorrect
+            .map((answer, i) => (
+              <button
+                data-testid={ `wrong-answer-${i}` }
+                key={ answer }
+                type="button"
+              >
+                {answer}
+              </button>
+            ))}
+        </section>
+        <button type="button" onClick={ this.handleNext }>Próxima</button>
+      </div>
+    );
+  }
+
   render() {
-    // const token = localStorage.getItem('token');
-    const fetchApi = getToken();
+    const { questions } = this.state;
+    console.log(questions);
     return (
       <div>
         <Header />
-        <p>SU CE SSO</p>
-        <div data-testid="question-category">
-          {console.log(fetchApi)}
-          <p>SU CE SSO</p>
-        </div>
+        {questions.length > 0 ? this.renderQuestions() : <p>Loading</p> }
       </div>
     );
   }
--- src/services/index.js
+++ src/services/index.js
@@ -1,20 +1,22 @@
 const API_URL = 'https://opentdb.com/api_token.php?command=request';
 
-const API_PERGUNTAS = 'https://opentdb.com/api.php?amount=5&token=9653cb44bb1a3d0db0beac46a203b8eb824a1e79c9078b165629f26b1daa490f';
+// const API_PERGUNTAS = 'https://opentdb.com/api.php?amount=5&token=9653cb44bb1a3d0db0beac46a203b8eb824a1e79c9078b165629f26b1daa490f';
 
 // const token = localStorage.getItem('token');
 
 export const getQuestions = () => {
-  fetch(API_PERGUNTAS)
+  const token = localStorage.getItem('token');
+  const url = `https://opentdb.com/api.php?amount=5&token=${token}`;
+
+  return fetch(url)
     .then((response) => response.json())
-    .then((resJson) => resJson);
+    .then((data) => data.results);
 };
 
 export const getToken = () => {
   fetch(API_URL)
     .then((response) => response.json())
     .then((data) => localStorage.setItem('token', data.token));
-  getQuestions();
 };
 
 export default getToken;
