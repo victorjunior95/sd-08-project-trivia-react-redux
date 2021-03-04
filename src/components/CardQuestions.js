27eaed9989e962b66e3a4c98d07530f508ba4969
Clênio Galvão <clenio.cgm@gmail.com>
2021-03-04 20:56:03 -0300

    resolve req 9

--- src/components/CardQuestions.js
+++ src/components/CardQuestions.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { updateScoreAction } from '../actions';
 
 const HALF_SHUFFLE = 0.5;
+const CONST_INIT = 10;
+const THREE = 3;
 class CardQuestions extends Component {
   constructor(props) {
     super(props);
@@ -10,12 +13,59 @@ class CardQuestions extends Component {
     this.state = {
       ask: 0,
       disabled: false,
+      timer: 30,
+      assertions: 0,
     };
     this.onclick = this.onclick.bind(this);
   }
 
-  onclick() {
+  componentDidMount() {
+    const player = JSON.stringify({
+      name: '',
+      assertions: 0,
+      score: 0,
+      gravatarEmail: '',
+      answer: '',
+    });
+    return (
+      localStorage.setItem('player', player)
+    );
+  }
+
+  onclick(answerUser) {
     this.setState({ disabled: true });
+    const { timer, ask, assertions, answer } = this.state;
+    const { updateScore, questionCard } = this.props;
+    let { score } = this.props;
+
+    let player = JSON.parse(localStorage.getItem('player'));
+    const { name, gravatarEmail } = player;
+
+    if (answerUser === answer) {
+      score += CONST_INIT + (timer * this.difficulty(questionCard[ask].difficulty));
+    }
+
+    player = JSON.stringify({ name, assertions, score, gravatarEmail });
+    updateScore(score);
+    return (
+      localStorage.setItem('player', player)
+    );
+  }
+
+  difficulty(dificuldade) {
+    if (dificuldade === 'easy') {
+      return 1;
+    }
+    if (dificuldade === 'medium') {
+      return 2;
+    }
+    return THREE;
+  }
+
+  decode(texto) {
+    const txt = document.createElement('textarea');
+    txt.innerHTML = texto;
+    return txt.value;
   }
 
   render() {
@@ -27,7 +77,9 @@ class CardQuestions extends Component {
           && (
             <div>
               <h3 data-testid="question-category">{ questionCard[ask].category }</h3>
-              <p data-testid="question-text">{ decodeURI(questionCard[ask].question) }</p>
+              <p data-testid="question-text">
+                { this.decode(questionCard[ask].question) }
+              </p>
               <div>
                 { [...questionCard[ask].incorrect_answers,
                   questionCard[ask].correct_answer].map((element, index) => {
@@ -36,12 +88,16 @@ class CardQuestions extends Component {
                   return (
                     <button
                       type="button"
-                      onClick={ this.onclick }
+                      onClick={ () => {
+                        this.setState({
+                          answer: questionCard[ask].correct_answer,
+                        }, () => this.onclick(element));
+                      } }
                       disabled={ disabled }
                       data-testid={ dataId() }
                       key={ element }
                     >
-                      { element }
+                      { this.decode(element) }
                     </button>
                   );
                 }).sort(() => Math.random() - HALF_SHUFFLE)}
@@ -60,10 +116,17 @@ class CardQuestions extends Component {
 
 CardQuestions.propTypes = {
   questionCard: PropTypes.arrayOf(PropTypes.object).isRequired,
+  score: PropTypes.number.isRequired,
+  updateScore: PropTypes.func.isRequired,
 };
+
+const mapDispatchToProps = (dispatch) => ({
+  updateScore: (score) => dispatch(updateScoreAction(score)),
+});
 
 const mapStateToProps = (state) => ({
   questionCard: state.play.questions,
+  score: state.play.score,
 });
 
-export default connect(mapStateToProps)(CardQuestions);
+export default connect(mapStateToProps, mapDispatchToProps)(CardQuestions);
