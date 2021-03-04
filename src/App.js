08a501e4675fdbb8811d0945879fa84c7d54097c
Lucas Gama <lucasbg@protonmail.com>
2021-03-04 19:58:35 -0300

    Requisito 11: ao final do jogo, redireciona para feedbacks

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Play from './pages/Play';
 import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
 import store from './store';
 import './App.css';
 
@@ -12,6 +13,7 @@ export default function App() {
     <BrowserRouter>
       <Provider store={ store }>
         <Switch>
+          <Route path="/feedback" component={ Feedback } />
           <Route path="/settings" component={ Settings } />
           <Route path="/play" component={ Play } />
           <Route path="/" component={ Home } />
--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 import Timer from '../components/Timer';
 import Questions from '../components/Questions';
 
 class Play extends React.Component {
   render() {
+    const { questions, questionPos } = this.props;
     return (
       <section>
         <Header />
-        <Timer />
-        <Questions />
+        { questionPos < questions.length || questions.length === 0
+          ? (
+            <div>
+              <Timer />
+              <Questions />
+            </div>
+          )
+          : <Redirect to="/feedback" /> }
       </section>
     );
   }
 }
 
-export default Play;
+Play.propTypes = {
+  questions: PropTypes.arrayOf(PropTypes.object).isRequired,
+  questionPos: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  questions: state.game.questions,
+  questionPos: state.game.questionPos,
+});
+
+export default connect(mapStateToProps)(Play);
