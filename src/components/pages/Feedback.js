65aa545cd89c77ffd5a70f3c5cd41afe25513939
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-03-04 11:29:24 -0300

    Req12 finalizado parcialmente

--- src/components/pages/Feedback.js
+++ src/components/pages/Feedback.js
@@ -1,46 +1,12 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import md5 from 'crypto-js/md5';
+import GameHeader from './GameHeader';
 
 class Feedback extends Component {
-  constructor() {
-    super();
-    this.convertEmail = this.createImage.bind(this);
-  }
-
-  createImage() {
-    const user = md5().toString();
-    const hash = `https://www.gravatar.com/avatar/${user}`;
-    return hash;
-  }
-
   render() {
-    const { name } = this.props;
-    console.log(name);
-
     return (
-      <header>
-        <div>
-          <img
-            src={ this.createImage() }
-            alt="userimage"
-            data-testid="header-profile-picture"
-          />
-          <p data-testid="header-player-name">
-            Usuário:
-            <span>{name}</span>
-          </p>
-          <p data-testid="header-score">
-            Placar:
-            <span>{}</span>
-          </p>
-        </div>
-      </header>
+      <GameHeader />
     );
   }
 }
-const mapStateToProps = (state) => ({
-  name: state.reducerUser.name,
-});
 
-export default connect(mapStateToProps)(Feedback);
+export default Feedback;
