c3abe637b155dc3d39fae61a99718c8cedf302c9
Daniel Leon <daniel.radialista@gmail.com>
2021-03-04 14:56:01 -0300

    corrigido linhas 6 e 7 do arquivo Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,8 +4,8 @@ import { connect } from 'react-redux';
 import { setLogin } from '../actions/index';
 
 class Login extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       email: '',
       name: '',
