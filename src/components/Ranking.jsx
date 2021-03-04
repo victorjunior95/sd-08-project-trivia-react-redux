880e07b8a956a1be8725a22e77b9777f3f0291d0
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-03-03 22:50:09 -0300

    Requisito 17

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -2,10 +2,39 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Ranking extends Component {
+  generateRanking() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+    ranking.sort((a, b) => b.score - a.score);
+    return (
+      ranking.map((e, index) => (
+        <tr
+          key={ index }
+          role="row"
+        >
+          <td role="cell">
+            <img src={ e.picture } alt="Gravatar" />
+          </td>
+          <td role="cell" data-testid={ `player-name-${index}` }>{e.name}</td>
+          <td role="cell" data-testid={ `player-score-${index}` }>{e.score}</td>
+        </tr>
+      ))
+    );
+  }
+
   render() {
     return (
       <div>
-        <h1 data-testid="ranking-title">Ranking</h1>
+        <h1 className="ranking" data-testid="ranking-title">Ranking</h1>
+        <table>
+          <tbody>
+            <tr>
+              <th>Foto</th>
+              <th>Jogador</th>
+              <th>Pontução</th>
+            </tr>
+            {this.generateRanking()}
+          </tbody>
+        </table>
       </div>
     );
   }
