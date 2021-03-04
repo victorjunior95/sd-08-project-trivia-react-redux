4a63749db9c7417cbf5ee7ae55eabaf266b70799
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-03-03 21:45:50 -0300

    Reqs: 14, 15, 16

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -2,30 +2,58 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import md5 from 'crypto-js/md5';
+import { Redirect } from 'react-router-dom';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+    this.state = {
+      redirect: false,
+      redirectTo: '',
+    };
+  }
+
   render() {
+    const { redirect, redirectTo } = this.state;
+    if (redirect) {
+      return <Redirect push to={ redirectTo } />;
+    }
+
     const GOOD_SCORE = 3;
-    const { name, score, email } = this.props;
+    const { name, score, email, assertions } = this.props;
+
     return (
       <div>
-        {console.log(name, score, email)}
-
         <img
           data-testid="header-profile-picture"
           src={ `https://www.gravatar.com/avatar/${md5(email)}` }
           alt="Gravatar"
           width="150px"
         />
+        <span data-testid="header-player-name">{`Nome: ${name}`}</span>
+        <span data-testid="header-score">{`Pontução: ${score}`}</span>
+
         <span data-testid="feedback-text">
           {score >= GOOD_SCORE ? 'Mandou bem!' : 'Podia ser melhor...'}
         </span>
-        <span data-testid="header-player-name">
-          {`Name: ${name}`}
-        </span>
-        <span data-testid="header-player-name">
-          {`Score: ${score}`}
-        </span>
+        <span>Acertos: </span>
+        <span data-testid="feedback-total-question">{assertions}</span>
+        <span>Pontução: </span>
+        <span data-testid="feedback-total-score">{score}</span>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ () => this.setState({ redirect: !redirect, redirectTo: '/' }) }
+        >
+          Jogar novamente
+        </button>
+        <button
+          type="button"
+          data-testid="btn-ranking"
+          onClick={ () => this.setState({ redirect: !redirect, redirectTo: '/ranking' }) }
+        >
+          Ver Ranking
+        </button>
       </div>
     );
   }
@@ -37,6 +65,7 @@ function mapStateToProps(state) {
     name: player.name,
     score: player.score,
     email: player.email,
+    assertions: player.assertions,
   };
 }
 
@@ -50,12 +79,14 @@ Feedback.propTypes = {
   name: PropTypes.string,
   email: PropTypes.string,
   score: PropTypes.number,
+  assertions: PropTypes.number,
 };
 
 Feedback.defaultProps = {
   name: '',
   score: 0,
   email: 'any@gmail.com',
+  assertions: 0,
 };
 
 export default connect(
