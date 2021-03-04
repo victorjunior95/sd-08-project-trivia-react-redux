454b280ca9c124c9ec9457703a88dc439ced4a6c
Caio Menezes Rocha <caiomzrocha@gmail.com>
2021-03-04 02:26:29 -0300

    [ADD] funçao para obter as questoes da API

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,4 @@
-export default async function getUserToken() {
+export async function getUserToken() {
   try {
     const token = await fetch('https://opentdb.com/api_token.php?command=request')
       .then((data) => data.json());
@@ -6,4 +6,11 @@ export default async function getUserToken() {
   } catch (error) {
     throw new Error(error);
   }
+}
+
+export function getQuestions(numberOfQuestions, token) {
+  const questions = fetch(`https://opentdb.com/api.php?amount=${numberOfQuestions}&token=${token}`)
+    .then((allQuestions) => allQuestions.json())
+    .catch((error) => { throw new Error(error); });
+  return questions;
 }
