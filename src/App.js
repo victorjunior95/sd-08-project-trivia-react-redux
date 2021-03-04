e8b293388ecb3f5c09da87afe8796afd8b2d80f6
Beto-CGF <beto.cgf@gmail.com>
2021-03-04 12:49:01 -0300

    main-group-24-Req3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Jogo from './pages/Jogo';
+import Set from './pages/Set';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Switch>
         <Route path="/jogo" render={ (props) => <Jogo { ...props } /> } />
         <Route exact path="/" render={ (props) => <Home { ...props } /> } />
+        <Route path="/set" render={ (props) => <Set { ...props } /> } />
       </Switch>
     </div>
   );
---
+++ src/pages/Set/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Set extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Configurações</h1>
+      </div>
+    );
+  }
+}
+
+export default Set;
