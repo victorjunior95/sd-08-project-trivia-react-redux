79c8354e7cb1d491b767bd2843a7eb2e13147f99
UmberBn <humberto-bandeira@hotmail.com>
2021-03-03 22:58:30 -0400

    Resolvido conflitos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import getToken from '../services/gravatar';
 import '../styles/Home.css';
-import { Redirect } from 'react-router-dom';
 import logo from '../trivia.png';
 import requestTriviaToken from '../services/API';
 
@@ -14,34 +13,10 @@ class Home extends React.Component {
       email: '',
       redirect: false,
     };
+
     this.handleInputsChanges = this.handleInputsChanges.bind(this);
     this.disableSubmit = this.disableSubmit.bind(this);
     this.playButtonOnClick = this.playButtonOnClick.bind(this);
-  }
-
-  setLocalStorageRanking(e) {
-    e.preventDefault();
-    const { username, email } = this.state;
-    const hash = getToken(email);
-    const pictureURL = `https://www.gravatar.com/avatar/${hash}`;
-    const ranking = {
-      name: username,
-      score: 0,
-      picture: pictureURL,
-    };
-
-  }
-
-  disableSubmit() {
-    const { username, email } = this.state;
-    return username.length === 0 || email.length === 0;
-  }
-
-    handleInputsChanges({ target }) {
-    const { id, value } = target;
-    this.setState({
-      [id]: value,
-    });
   }
 
   async playButtonOnClick(e) {
@@ -49,16 +24,28 @@ class Home extends React.Component {
     const { username, email } = this.state;
     const hash = getToken(email);
     const pictureURL = `https://www.gravatar.com/avatar/${hash}`;
+    const triviaToken = await requestTriviaToken();
     const ranking = {
       name: username,
       score: 0,
       picture: pictureURL,
     };
-    const triviaToken = await requestTriviaToken();
     localStorage.setItem('token', triviaToken);
     localStorage.setItem('ranking', JSON.stringify(ranking));
     this.setState({
       redirect: true,
+    });
+  }
+
+  disableSubmit() {
+    const { username, email } = this.state;
+    return username.length === 0 || email.length === 0;
+  }
+
+  handleInputsChanges({ target }) {
+    const { id, value } = target;
+    this.setState({
+      [id]: value,
     });
   }
 
