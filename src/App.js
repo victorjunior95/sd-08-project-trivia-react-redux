89a133995ff4b10ff74eaa0f2211dc7cd4cc607c
UmberBn <humberto-bandeira@hotmail.com>
2021-03-03 22:11:39 -0400

    Requisito concluido

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-
 import Home from './pages/Home';
+import Game from './pages/Game';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Home } />
+      <Route path="/game" component={ Game } />
     </Switch>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,39 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import getToken from '../services/gravatar';
 import '../styles/Home.css';
 import logo from '../trivia.png';
 
 class Home extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       username: '',
       email: '',
+      redirect: false,
     };
     this.handleInputsChanges = this.handleInputsChanges.bind(this);
     this.disableSubmit = this.disableSubmit.bind(this);
+    this.setLocalStorageRanking = this.setLocalStorageRanking.bind(this);
+  }
+
+  setLocalStorageRanking(e) {
+    e.preventDefault();
+    const { username, email } = this.state;
+    const hash = getToken(email);
+    const pictureURL = `https://www.gravatar.com/avatar/${hash}`;
+    const ranking = {
+      name: username,
+      score: 0,
+      picture: pictureURL,
+    };
+    localStorage.setItem('ranking', JSON.stringify(ranking));
+    this.setState({ redirect: true });
+  }
+
+  disableSubmit() {
+    const { username, email } = this.state;
+    return username.length === 0 || email.length === 0;
   }
 
   handleInputsChanges({ target }) {
@@ -20,44 +43,44 @@ class Home extends React.Component {
     });
   }
 
-  disableSubmit() {
-    const { username, email } = this.state;
-    return username.length === 0 || email.length === 0;
-  }
-
   render() {
-    const { username, email } = this.state;
+    const { username, email, redirect } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <form>
-            <input
-              id="username"
-              data-testid="input-player-name"
-              type="text"
-              value={ username }
-              placeholder="Nome de usuário"
-              onChange={ this.handleInputsChanges }
-            />
-            <input
-              id="email"
-              data-testid="input-gravatar-email"
-              type="email"
-              value={ email }
-              placeholder="seu@email.com"
-              onChange={ this.handleInputsChanges }
-            />
-            <button
-              type="submit"
-              data-testid="btn-play"
-              disabled={ this.disableSubmit() }
-            >
-              Jogar
-            </button>
-          </form>
-        </header>
-      </div>
+      redirect
+        ? (<Redirect to="/game" />)
+        : (
+          <div className="App">
+            <header className="App-header">
+              <img src={ logo } className="App-logo" alt="logo" />
+              <form>
+                <input
+                  id="username"
+                  data-testid="input-player-name"
+                  type="text"
+                  value={ username }
+                  placeholder="Nome de usuário"
+                  onChange={ this.handleInputsChanges }
+                />
+                <input
+                  id="email"
+                  data-testid="input-gravatar-email"
+                  type="email"
+                  value={ email }
+                  placeholder="seu@email.com"
+                  onChange={ this.handleInputsChanges }
+                />
+                <button
+                  type="submit"
+                  data-testid="btn-play"
+                  disabled={ this.disableSubmit() }
+                  onClick={ this.setLocalStorageRanking }
+                >
+                  Jogar
+                </button>
+              </form>
+            </header>
+          </div>
+        )
     );
   }
 }
