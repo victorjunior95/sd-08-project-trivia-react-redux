d1939d416012cf255a74162afdad28d6f5a48ec2
Clênio Galvão <clenio.cgm@gmail.com>
2021-03-04 11:20:08 -0300

    inclui css no Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,48 +18,69 @@ class Login extends Component {
     const { login, token } = this.props;
     const { nameUser, emailUser } = this.state;
     return (
-      <form>
-        <label htmlFor="name-input">
-          Nome do Jogador
-          <input
-            id="name-input"
-            value={ nameUser }
-            onChange={ (e) => this.setState({ nameUser: e.target.value }) }
-            data-testid="input-player-name"
-          />
-        </label>
-        <label htmlFor="email-input">
-          Email do Gravatar
-          <input
-            id="email-input"
-            value={ emailUser }
-            onChange={ (e) => this.setState({ emailUser: e.target.value }) }
-            data-testid="input-gravatar-email"
-          />
-        </label>
-        <Link to="/game">
-          <button
-            type="button"
-            data-testid="btn-play"
-            disabled={
-              !(nameUser.length > 0 && /^[a-z0-9.]+@[a-z0-9]+\.[a-z]/.test(emailUser))
-            }
-            onClick={ () => {
-              login(this.state);
-              token();
-            } }
-          >
-            Jogar
-          </button>
-        </Link>
-        <div>
-          <Link to="/settings">
-            <button type="button" data-testid="btn-settings">
-              Settings
-            </button>
-          </Link>
+      <div className="d-flex justify-content-center mt-5">
+        <div className="card text-center w-50">
+          <h3 className="card-header">
+            Login
+          </h3>
+          <div className="d-flex flex-column card-body">
+            <form className="d-flex flex-column">
+              <label htmlFor="name-input" className="d-flex">
+                <span className="input-group-text">
+                  Nome do Jogador
+                </span>
+                <input
+                  id="name-input"
+                  value={ nameUser }
+                  onChange={ (e) => this.setState({ nameUser: e.target.value }) }
+                  data-testid="input-player-name"
+                  className="form-control"
+                />
+              </label>
+              <label htmlFor="email-input" className="d-flex">
+                <span className="input-group-text">
+                  Email do Gravatar
+                </span>
+                <input
+                  id="email-input"
+                  value={ emailUser }
+                  onChange={ (e) => this.setState({ emailUser: e.target.value }) }
+                  data-testid="input-gravatar-email"
+                  className="form-control"
+                />
+              </label>
+              <Link to="/game">
+                <button
+                  type="button"
+                  data-testid="btn-play"
+                  disabled={
+                    !(nameUser.length > 0 && /^[a-z0-9.]+@[a-z0-9]+\.[a-z]/
+                      .test(emailUser))
+                  }
+                  onClick={ () => {
+                    login(this.state);
+                    token();
+                  } }
+                  className="btn btn-primary"
+                >
+                  Jogar
+                </button>
+              </Link>
+            </form>
+          </div>
+          <div className="card-footer">
+            <Link to="/settings">
+              <button
+                type="button"
+                data-testid="btn-settings"
+                className="btn btn-secondary"
+              >
+                Settings
+              </button>
+            </Link>
+          </div>
         </div>
-      </form>
+      </div>
     );
   }
 }
