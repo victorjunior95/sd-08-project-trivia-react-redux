c2a6723159407a91729c0f3cde11b59cacb3bf5c
amandaos <amanda.775.souza@gmail.com>
2021-03-04 13:08:13 -0300

    feature: finalizando requisitos 12 e 13, estruturando o 14

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,20 +5,19 @@ import Header from '../components/Header';
 const MINIMUM_ASSERTIONS = 3;
 
 class Feedback extends Component {
-  feedback() {
-    const assertions = localStorage.getItem('assertions');
-    if (assertions < MINIMUM_ASSERTIONS) {
-      return <span data-testid="feedback-text">Podia ser melhor...</span>;
-    } if (assertions >= MINIMUM_ASSERTIONS) {
-      return <span data-testid="feedback-text">Mandou bem!</span>;
-    }
-  }
-
   render() {
+    const assertions = localStorage.getItem('assertions') || 0;
+    const totalQuestion = 5;
+    const texto = assertions < MINIMUM_ASSERTIONS ? 'Podia ser melhor...' : 'Mandou bem!';
+
     return (
       <>
         <Header />
-        { this.feedback() }
+        <span data-testid="feedback-text">{texto}</span>
+        <span data-testid="feedback-total-score">{assertions}</span>
+        /
+        <span data-testid="feedback-total-question">{totalQuestion}</span>
+
       </>
     );
   }
