b452330471731ee727ec30b5ab938b63aa81213f
UmberBn <humberto-bandeira@hotmail.com>
2021-03-03 23:18:59 -0400

    Subindo documentos que nao foram enviados

---
+++ src/pages/Game.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from '../common/components/Header/Header';
+
+class Game extends Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+
+export default Game;
