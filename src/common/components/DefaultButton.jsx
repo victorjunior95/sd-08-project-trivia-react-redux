7b249acfb3e1ffefffc63c65813db07b1d2b51c9
Max <gmaxgomes@gmail.com>
2021-03-04 12:44:48 -0300

    G25 - Turns DefaultButton into class component and adds isSubmit to defaultProps

--- src/common/components/DefaultButton.jsx
+++ src/common/components/DefaultButton.jsx
@@ -1,31 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const DefaultButton = (props) => {
-  const { reqAttributes, btnText, name, isSubmit = undefined, disabled = false } = props;
-  return (
-    <button
-      type={ isSubmit ? 'submit' : 'button' }
-      data-testid={ reqAttributes }
-      name={ name }
-      disabled={ disabled }
-    >
-      {btnText}
-    </button>
-  );
-};
+class DefaultButton extends Component {
+  render() {
+    const {
+      reqAttributes,
+      btnText,
+      name,
+      isSubmit = undefined,
+      disabled = false,
+    } = this.props;
+    return (
+      <button
+        type={ isSubmit ? 'submit' : 'button' }
+        data-testid={ reqAttributes }
+        name={ name }
+        disabled={ disabled }
+      >
+        {btnText}
+      </button>
+    );
+  }
+}
 
 DefaultButton.propTypes = {
   reqAttributes: PropTypes.string.isRequired,
   btnText: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  isSubmit: PropTypes.string.isRequired,
+  isSubmit: PropTypes.string,
   disabled: PropTypes.bool,
 
 };
 
 DefaultButton.defaultProps = {
   disabled: false,
+  isSubmit: undefined,
 };
 
 export default DefaultButton;
