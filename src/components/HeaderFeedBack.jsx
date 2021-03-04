26842bc4bf9bba675ace07c817f529b80d93dcff
Uilton Rocha <itoclick@gmail.com>
2021-03-03 22:33:03 -0300

    Requisito 12

--- src/components/HeaderFeedBack.jsx
+++ src/components/HeaderFeedBack.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 
-// const url = 'https://www.gravatar.com/avatar/5a434b307b728b98080610414c6788497df4e9ed';
-/* const getimg = async () => {
-  const fatchimg = await fetch(url)
-    .then((response) => response.json());
-    .then((data)=>data.img )
-
-}; */
 function HeaderFeedBack() {
+  const url = "https://s.gravatar.com/avatar/6791ab1c56a700f2fc41e7fa358deeeb?s=80";
+
   return (
     <head>
-      <img src="https://s.gravatar.com/avatar/6791ab1c56a700f2fc41e7fa358deeeb?s=80" alt="imagem avatar" data-testid="header-profile-picture" />
-      <span data-testid="header-player-name">{okok}</span>
-      <span data-testid="header-score">{ok}</span>
-      <span>{okok}</span>
+      <img src={ url } alt="imagem avatar" data-testid="header-profile-picture" />
+      <span data-testid="header-player-name">{payload.name}</span>
+      <span data-testid="header-score">{payload.score}</span>
     </head>
   );
 }
