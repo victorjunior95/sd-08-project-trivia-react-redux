ab663f67fd1106eeed3d61e1837418b95f496d03
Uilton Rocha <itoclick@gmail.com>
2021-03-03 22:40:24 -0300

    Requisito 12

--- src/components/HeaderFeedBack.jsx
+++ src/components/HeaderFeedBack.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function HeaderFeedBack() {
-  const url = "https://s.gravatar.com/avatar/6791ab1c56a700f2fc41e7fa358deeeb?s=80";
+export default function HeaderFeedBack() {
+  const url = 'https://s.gravatar.com/avatar/6791ab1c56a700f2fc41e7fa358deeeb?s=80';
 
   return (
     <head>
