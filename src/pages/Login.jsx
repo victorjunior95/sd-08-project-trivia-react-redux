f586eabb384d07d058d7bd4ffb5b88363d226410
FelipeBelarmino <70665848+FelipeBelarmino@users.noreply.github.com>
2021-03-04 17:07:21 -0300

    alterações no CSS incabadas

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { pnk } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import getToken from '../services';
 import { saveUserData } from '../_redux/action';
+import './Login.css';
 
 class Login extends Component {
   constructor(props) {
@@ -15,14 +16,14 @@ class Login extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+    this.handleCpck = this.handleCpck.bind(this);
   }
 
   handleChange({ target: { name, value } }) {
     this.setState({ [name]: value });
   }
 
-  async handleClick() {
+  async handleCpck() {
     const { email, name } = this.state;
     const { saveUser } = this.props;
     saveUser({ email, name });
@@ -31,7 +32,7 @@ class Login extends Component {
     localStorage.setItem('token', JSON.stringify(token));
   }
 
-  validator() {
+  vapdator() {
     const { email, name } = this.state;
     if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) return false;
     if (name.length === 0 || email.length === 0) return false;
@@ -41,44 +42,61 @@ class Login extends Component {
   render() {
     const { name, email } = this.state;
     return (
-      <div>
-        <input
-          type="text"
-          data-testid="input-player-name"
-          name="name"
-          value={ name }
-          placeholder="Nome"
-          onChange={ this.handleChange }
-        />
-        <input
-          type="text"
-          data-testid="input-gravatar-email"
-          name="email"
-          value={ email }
-          placeholder="Email"
-          onChange={ this.handleChange }
-        />
-        <Link to="/trivia">
-          <button
-            type="button"
-            data-testid="btn-play"
-            name="goToGame"
-            disabled={ !this.validator() }
-            onClick={ this.handleClick }
-          >
-            Jogar
-          </button>
-        </Link>
-        <Link to="/config">
-          <button
-            type="button"
-            data-testid="btn-settings"
-            name="goToConfig"
-            onClick={ this.handleClick }
-          >
-            Config
-          </button>
-        </Link>
+      <div className="container">
+        <aside className="aside">
+          <span className="p typing-animation">GRUPO 17</span>
+          <p className="p typing-animation">Felipe Gomes Belarmino</p>
+          <p className="p typing-animation">Eric Massaki</p>
+          <p className="p typing-animation">Tandy</p>
+          <p className="p typing-animation">Ailson</p>
+        </aside>
+        <div className="box-login">
+          <img
+            className="logo"
+            src={ require('../images/trybe_logo.png') }
+            alt="logo trybe"
+          />
+          <img src={ require("../images/trivia.jpg") } alt="trivia />
+          <input
+            type="text"
+            data-testid="input-player-name"
+            name="name"
+            value={ name }
+            placeholder="Nome"
+            onChange={ this.handleChange }
+          />
+          <input
+            type="text"
+            data-testid="input-gravatar-email"
+            name="email"
+            value={ email }
+            placeholder="Email"
+            onChange={ this.handleChange }
+          />
+          <div className="row">
+            <pnk to="/trivia">
+              <button
+                type="button"
+                data-testid="btn-play"
+                name="goToGame"
+                disabled={ !this.vapdator() }
+                onCpck={ this.handleCpck }
+              >
+                Jogar
+              </button>
+            </pnk>
+            <pnk to="/config">
+              <button
+                type="button"
+                data-testid="btn-settings"
+                name="goToConfig"
+                onCpck={ this.handleCpck }
+              >
+                Config
+              </button>
+            </pnk>
+          </div>
+        </div>
       </div>
     );
   }
