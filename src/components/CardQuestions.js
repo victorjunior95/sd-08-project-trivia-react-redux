49e53b559cd0993417a617a5ba1d8ad57aaa20d5
gabrielDonnantuoni <gabrieldonnantuoni@gmail.com>
2021-03-04 16:38:31 -0300

    Resolve final lint problems

--- src/components/CardQuestions.js
+++ src/components/CardQuestions.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-
+const HALF_SHUFFLE = 0.5;
 class CardQuestions extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+
     this.state = {
       ask: 0,
       disabled: false,
-    }
+    };
     this.onclick = this.onclick.bind(this);
   }
 
@@ -22,16 +23,16 @@ class CardQuestions extends Component {
     const { ask, disabled } = this.state;
     return (
       <div>
-        { questionCard.length > 0 &&
-          <div>
-            <h3 data-testid="question-category">{ questionCard[ask].category }</h3>
-            <p data-testid="question-text">{ decodeURI(questionCard[ask].question) }</p>
+        { questionCard.length > 0
+          && (
             <div>
-            { [ ...questionCard[ask].incorrect_answers,
-                  questionCard[ask].correct_answer ]
-                .map((element, index) => {
-                  const dataId = () => element === questionCard[ask].correct_answer ?
-                    "correct-answer" : `wrong-answer-${index}`;
+              <h3 data-testid="question-category">{ questionCard[ask].category }</h3>
+              <p data-testid="question-text">{ decodeURI(questionCard[ask].question) }</p>
+              <div>
+                { [...questionCard[ask].incorrect_answers,
+                  questionCard[ask].correct_answer].map((element, index) => {
+                  const dataId = () => (element === questionCard[ask].correct_answer
+                    ? 'correct-answer' : `wrong-answer-${index}`);
                   return (
                     <button
                       type="button"
@@ -43,27 +44,25 @@ class CardQuestions extends Component {
                       { element }
                     </button>
                   );
-                }).sort(() => Math.random() - 0.5)
-              }
-            </div>
+                }).sort(() => Math.random() - HALF_SHUFFLE)}
+              </div>
               <button
-              type="button"
-              onClick={ () => this.setState({ ask: ask + 1 }) }
+                type="button"
+                onClick={ () => this.setState({ ask: ask + 1 }) }
               >
                 Next
               </button>
-          </div>
+            </div>)}
+      </div>
+    );
   }
-  </div>
-    )
-}}
-
+}
 
 CardQuestions.propTypes = {
   questionCard: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-const mapStateToProps = (state) =>({
+const mapStateToProps = (state) => ({
   questionCard: state.play.questions,
 });
 
