26d62ab92ee89595180b7317496438283f125aab
Daniel <danieldsrd@gmail.com>
2021-03-04 13:28:25 -0300

    Requisitos: 7 e 10

--- src/pages/Trivia.js
+++ src/pages/Trivia.js
@@ -2,15 +2,30 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { fetchTriviaAPI as fetchTriviaAPIAction } from '../Redux/actions';
+import './Trivia.css';
+
+const INITIAL_STATE = {
+  btnTrue: '',
+  btnFalse: '',
+  disabled: false,
+  nextQuestion: false,
+};
 
 class Trivia extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       loading: true,
       question: [],
+      btnTrue: '',
+      btnFalse: '',
+      disabled: false,
+      number: 0,
+      nextQuestion: false,
     };
     this.renderQuestions = this.renderQuestions.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleNextQuestion = this.handleNextQuestion.bind(this);
   }
 
   async componentDidMount() {
@@ -18,6 +33,23 @@ class Trivia extends React.Component {
     const token = localStorage.getItem('token');
     await fetchTriviaAPI(token);
     await this.loadingData();
+  }
+
+  handleClick() {
+    this.setState({
+      btnTrue: 'button-true',
+      btnFalse: 'button-false',
+      disabled: true,
+      nextQuestion: true,
+    });
+  }
+
+  handleNextQuestion() {
+    const { number } = this.state;
+    this.setState({
+      ...INITIAL_STATE,
+      number: number + 1,
+    });
   }
 
   async loadingData() {
@@ -30,40 +62,62 @@ class Trivia extends React.Component {
     }
   }
 
-  renderQuestions() {
-    const { question } = this.state;
-    console.log(question[0]);
+  renderButtonNext() {
     return (
-      <>
-        <h3
-          data-testid="question-category"
-        >
-          {question[0].category}
-        </h3>
-        <span
-          data-testid="question-text"
-        >
-          {question[0].question}
-        </span>
-        <div>
-          <button
-            type="button"
-            data-testid="correct-answer"
-          >
-            {question[0].correct_answer}
-          </button>
-          {question[0].incorrect_answers.map((answer, index) => (
-            <button
-              key={ index }
-              type="button"
-              data-testid={ `wrong-answer-${index}` }
-            >
-              {answer}
-            </button>
-          ))}
-        </div>
-      </>
+      <button
+        type="button"
+        data-testid="btn-next"
+        onClick={ this.handleNextQuestion }
+      >
+        Próxima
+      </button>
     );
+  }
+
+  renderQuestions() {
+    const { question, btnTrue, btnFalse, disabled, number, nextQuestion } = this.state;
+    const FIVE_QUESTIONS = 4;
+    if (number <= FIVE_QUESTIONS) {
+      return (
+        <>
+          <h3
+            data-testid="question-category"
+          >
+            {question[number].category}
+          </h3>
+          <span
+            data-testid="question-text"
+          >
+            {question[number].question}
+          </span>
+          <div>
+            <button
+              type="button"
+              className={ btnTrue }
+              data-testid="correct-answer"
+              onClick={ this.handleClick }
+              disabled={ disabled }
+            >
+              {question[number].correct_answer}
+            </button>
+            {question[number].incorrect_answers.map((answer, index) => (
+              <button
+                key={ index }
+                type="button"
+                data-testid={ `wrong-answer-${index}` }
+                onClick={ this.handleClick }
+                className={ btnFalse }
+                disabled={ disabled }
+              >
+                {answer}
+              </button>
+            ))}
+            {nextQuestion ? this.renderButtonNext() : <div />}
+          </div>
+        </>
+      );
+    }
+    return <div />;
   }
 
   render() {
