56d17714f3094ec2b80e4d070a989f31b81a1f85
Seu nome <luiza.dilly@gmail.com>
2021-03-05 16:18:50 -0300

    req 15

--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { history } = this.props;
     const feedback = JSON.parse(localStorage.getItem('state')).player;
     return (
       <div>
@@ -12,10 +14,23 @@ class Feedback extends Component {
           : <p data-testid="feedback-text">Mandou bem!</p>}
         <p data-testid="feedback-total-score">{feedback.score}</p>
         <p data-testid="feedback-total-question">{feedback.assertions}</p>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ () => history.push('/') }
+        >
+          Jogar novamente
 
+        </button>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Feedback;
