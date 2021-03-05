052b98fedadfc02615bbdb1d85cb358503b77ce2
Jorge Alves <alvesdjorge@gmail.com>
2021-03-05 18:31:23 -0300

    req 13 com

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
-export default class Feedback extends Component {
+class Feedback extends Component {
   render() {
+    const certo = 3;
+    const { acertos, score } = this.props;
+    const messagem = acertos >= certo ? 'Mandou bem!' : 'Podia ser melhor...';
     return (
       <>
         <Header />
         <main>
+          <p data-testid="feedback-text">{mensagem}</p>
+          <p data-testid="feedback-total-question">
+            Você acertou:
+            {score}
+            perguntas.
+          </p>
+          <p data-testid="feedback-total-score">
+            Seu placar foi:
+            {acertos}
+          </p>
           <p data-testid="feedback-total-score">Seu placar foi: 0.</p>
           <p data-testid="feedback-total-question">Você acertou: 0 perguntas.</p>
-          <p data-testid="feedback-total-score">Seu placar foi: 0.</p>
-          <p data-testid="feedback-total-question">Você acertou: 0 perguntas.</p>
+
+          <p data-testid="feedback-text">Mandou bem!</p>
+          <p data-testid="feedback-text">Podia ser melhor ....</p>
 
         </main>
       </>
     );
   }
 }
+
+Feedback.propTypes = {
+  acertos: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  acertos: state.user.player.assertions,
+  score: state.user.player.score,
+});
+
+export default connect(mapStateToProps)(Feedback);
