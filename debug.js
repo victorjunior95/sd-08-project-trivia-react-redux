e65e618e89e73d351cb07026cd87793b8b90ff19
wesleypribeiro <wesley_paulo00@hotmail.com>
2021-03-05 20:38:24 -0300

    R05, R06 - Concluido

--- debug.js
+++
@@ -1,2 +0,0 @@
-const teste = {category: "History", type: "boolean", difficulty: "easy", question: "The Cold War ended with Joseph Stalin&#039;s death.", correct_answer: "False"};
-console.log(teste.question);
