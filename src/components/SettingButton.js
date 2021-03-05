77533abb621ea6fc444a86c38169d710d8f7f81d
Arthur Nunes <coletivosuburbano@gmail.com>
2021-03-05 18:30:24 -0300

    eslint fix

--- src/components/SettingButton.js
+++ src/components/SettingButton.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export const SettingButton =() => {
-    return(
-<Link to="/settings">
-<button 
-  data-testid="btn-settings"
-  type="button"
-//   onClick={ }
->
-  Setting
-</button>
-</Link>
-    )}
+export default class SettingButton extends Component {
+  render() {
+    return (
+      <Link to="/settings">
+        <button
+          data-testid="btn-settings"
+          type="button"
+        >
+          Setting
+        </button>
+      </Link>
+    );
+  }
+}
