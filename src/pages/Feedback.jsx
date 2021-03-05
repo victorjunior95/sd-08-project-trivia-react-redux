6a77cd265dbdf869e20a2290f3a8bbe606d5ba3a
Lucas Gama <lucasbg@protonmail.com>
2021-03-05 16:15:42 -0300

    Requisitos 12 e 13: informações e mensagem na pagina de feedback

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Feedback extends React.Component {
   render() {
+    const { img, name, score, assertions } = this.props;
+    const minAssertions = 3;
     return (
-      <h1 data-testid="feedback-text">Feedback</h1>
+      <section>
+        <header>
+          <img src={ img } alt="avatar" data-testid="header-profile-picture" />
+          <p data-testid="header-player-name">{ name }</p>
+          <p data-testid="header-score">{ score }</p>
+        </header>
+        <p data-testid="feedback-text">
+          { assertions < minAssertions ? 'Podia ser melhor...' : 'Mandou bem!' }
+        </p>
+      </section>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  img: state.user.urlPicture,
+  name: state.user.name,
+  score: state.game.score,
+  assertions: state.game.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
