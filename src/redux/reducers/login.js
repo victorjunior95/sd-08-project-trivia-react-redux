6b9cb15ceb051c804d0892889a680db3c7614ec6
Victor Miwa <victoryukimiwa@hotmail.com>
2021-03-05 18:25:42 -0300

    Tela de login com botao da api

---
+++ src/redux/reducers/login.js
@@ -0,0 +1,25 @@
+const INITIAL_STATE = {
+  api: '',
+  error: '',
+  login: false,
+};
+
+const login = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'API_SUCCESS':
+    return {
+      ...state,
+      api: action.payload,
+      login: true,
+    };
+  case 'API_FAIL':
+    return {
+      ...state,
+      error: action.payload,
+    };
+  default:
+    return state;
+  }
+};
+
+export default login;
