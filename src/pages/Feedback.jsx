32f926a9540df153ed05a855958e0d3ae76657e9
Jorge Alves <alvesdjorge@gmail.com>
2021-03-05 16:09:19 -0300

    req 13

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+export default class Feedback extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <main>
+          <p data-testid="feedback-total-score">Seu placar foi: 0.</p>
+          <p data-testid="feedback-total-question">Você acertou: 0 perguntas.</p>
+          <p data-testid="feedback-text">Mandou bem!</p>
+          <p data-testid="feedback-text">Podia ser melhor ....</p>
+        </main>
+      </>
+    );
+  }
+}
