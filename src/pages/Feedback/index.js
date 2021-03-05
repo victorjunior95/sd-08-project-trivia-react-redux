5fb5d948b7bb656ff3c97271c70c9d4d2329d40d
Bruno Martins <bruno.martins217@gmail.com>
2021-03-05 17:53:11 -0300

    :package: Adicionando Requisito 14

--- src/pages/Feedback/index.js
+++ src/pages/Feedback/index.js
@@ -4,6 +4,8 @@ class Feedback extends Component {
   constructor() {
     super();
     this.getMessage = this.getMessage.bind(this);
+    this.getTotalScore = this.getTotalScore.bind(this);
+    this.getTotalAssertions = this.getTotalAssertions.bind(this);
   }
 
   getMessage() {
@@ -15,11 +17,33 @@ class Feedback extends Component {
     if (assertions >= acertos) return <p>Mandou bem!</p>;
   }
 
+  getTotalScore() {
+    const stringPlayerInfo = localStorage.getItem('state');
+    const playerInfo = JSON.parse(stringPlayerInfo);
+    const { player: { score } } = playerInfo;
+    return score;
+  }
+
+  getTotalAssertions() {
+    const stringPlayerInfo = localStorage.getItem('state');
+    const playerInfo = JSON.parse(stringPlayerInfo);
+    const { player: { assertions } } = playerInfo;
+    return assertions;
+  }
+
   render() {
     return (
-      <div data-testid="feedback-text">
-        {this.getMessage()}
-      </div>
+      <>
+        <div data-testid="feedback-text">
+          {this.getMessage()}
+        </div>
+        <div data-testid="feedback-total-score">
+          {this.getTotalScore()}
+        </div>
+        <div data-testid="feedback-total-question">
+          {this.getTotalAssertions()}
+        </div>
+      </>
     );
   }
 }
