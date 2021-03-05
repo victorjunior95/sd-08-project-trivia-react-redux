585b8bf8906a1643d2eaba4553f87cff604d91cb
Fernanda PC <nandateixmarques@gmail.com>
2021-03-05 19:50:44 -0300

    adicionando requisito 12

---
+++ src/pages/Feedback/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HeaderFeedback from '../../components/HeaderFeedback';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <div>
+        <HeaderFeedback />
+      </div>
+    );
+  }
+}
+
+export default Feedback;
