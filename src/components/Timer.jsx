d28a0a3205c24e2b1ae21d12f3bcd35681059c13
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-03-05 19:23:24 -0300

    Requisitos 7 e 8

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,30 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Timer extends Component {
   constructor() {
     super();
     this.state = {
-      timer: 30,
-      maxTimer: 30,
+      timer: 3,
       countingDown: false,
     };
 
-    this.handleClick = this.handleClick.bind(this);
+    this.startTimer = this.startTimer.bind(this);
   }
 
-  handleClick() {
+  componentDidMount() {
+    this.startTimer();
+  }
+
+  startTimer() {
+    const { handleClick } = this.props;
     const { countingDown } = this.state;
     const ONE_SECOND = 1000;
     if (!countingDown) {
       this.setState({ countingDown: true });
       const answerTimer = setInterval(() => {
-        const { timer, maxTimer } = this.state;
+        const { timer } = this.state;
         if (timer > 0) {
           this.setState({ timer: timer - (ONE_SECOND / ONE_SECOND) });
           return;
         }
         clearInterval(answerTimer);
-        this.setState({ timer: maxTimer, countingDown: false });
+        const expired = true;
+        handleClick(expired);
       }, ONE_SECOND);
     }
   }
@@ -40,5 +46,9 @@ class Timer extends Component {
     );
   }
 }
+
+Timer.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
 
 export default Timer;
