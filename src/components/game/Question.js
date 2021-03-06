0e820cbf2450c3032d109bc5fc1f10885187cec2
wesleypribeiro <wesley_paulo00@hotmail.com>
2021-03-06 14:30:45 -0300

    R07- Concluído

--- src/components/game/Question.js
+++ src/components/game/Question.js
@@ -3,10 +3,29 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import getQuestions from '../../actions/question';
 import { getAPIQuestions } from '../../services/trivia';
+import './styles.css';
 
 class Question extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      correct: '',
+      wrong: '',
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
   componentDidMount() {
     this.fetchQuestions();
+  }
+
+  onClick() {
+    this.setState({
+      correct: 'correct',
+      wrong: 'wrong',
+    });
   }
 
   async fetchQuestions() {
@@ -16,6 +35,7 @@ class Question extends React.Component {
   }
 
   render() {
+    const { correct, wrong } = this.state;
     const counter = 0;
     const { question } = this.props;
     if (!question.length) return <p>Loading...</p>;
@@ -25,29 +45,47 @@ class Question extends React.Component {
       <div>
         <p data-testid="question-category">{question[counter].category}</p>
         <p data-testid="question-text">{question[counter].question}</p>
-        {console.log(question[counter])}
         { (answers.length === 2)
           ? (
             <div>
-              <button type="button" data-testid="correct-answer">{answers[0]}</button>
-              <button type="button" data-testid="wrong-answer">{answers[1]}</button>
+              <button
+                type="button"
+                data-testid="correct-answer"
+                onClick={ this.onClick }
+                className={ correct }
+              >
+                {answers[0]}
+              </button>
+              <button
+                type="button"
+                data-testid="wrong-answer"
+                onClick={ this.onClick }
+                className={ wrong }
+              >
+                {answers[1]}
+              </button>
             </div>
           )
-          : answers.map((answer) => {
+          : answers.map((answer, index) => {
             if (answer === answers[0]) {
               return (
                 <button
+                  key={ index }
                   type="button"
                   data-testid="correct-answer"
+                  onClick={ this.onClick }
+                  className={ correct }
                 >
                   {answer}
                 </button>);
             }
             return (
               <button
-                key={ answer }
+                key={ index }
                 type="button"
                 data-testid="wrong-answer"
+                onClick={ this.onClick }
+                className={ wrong }
               >
                 {answer}
               </button>);
