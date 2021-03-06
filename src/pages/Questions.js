b5eacf5de350f5d08a2d7b5f9491bc2e5d0cfe85
hugobrr <hugobrr@gmail.com>
2021-03-05 21:40:15 -0300

    rascunhos de sabado a noite

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import  React,{ Component, useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import { actionLoadedQuestions } from '../actions/triviaActions';
+
+const THIRTY_SECONDS = 30000;
+const FIVE_SECONDS = 5000;
+const HALF_SECOND = 500;
 
 class Questions extends Component {
   constructor() {
@@ -12,8 +16,13 @@ class Questions extends Component {
       token: localStorage.getItem('token'),
       currentQuestion: 0,
       goToFeedback: false,
+      startTimer: 0,
+      isTimeout: false,
     };
     this.handleClick = this.handleClick.bind(this);
+    this.startTime = this.startTime.bind(this);
+    this.showTime = this.showTime.bind(this);
+    this.showTime = this.showTime.bind(this);
   }
 
   componentDidMount() {
@@ -22,10 +31,23 @@ class Questions extends Component {
     loadedQuestions(token);
   }
 
+//   useEffect(() => {
+//     const timer = setInterval(() => { // Creates an interval which will update the current data every minute
+//     // This will trigger a rerender every component that uses the useDate hook.
+//     setDate(new Date());
+//   }, 60 * 1000);
+//   return () => {
+//     clearInterval(timer); // Return a funtion to clear the timer so that it will stop being called on unmount
+//   }
+// }, []);
+
   handleClick() {
     const { currentQuestion } = this.state;
     const { resultQuestions = [] } = this.props;
     const amountAnswers = resultQuestions.length - 1;
+    this.startTime();
+    this.setState({ isTimeout: false })
+
     if (currentQuestion < amountAnswers) {
       this.setState({ currentQuestion: currentQuestion + 1 });
     } else {
@@ -33,22 +55,46 @@ class Questions extends Component {
     }
   }
 
-  // https://stackoverflow.com/a/42182294/14424360
-  decode(html) {
-    const txt = document.createElement('textarea');
-    txt.innerHTML = html;
-    return txt.value;
+  startTime(){
+   const now = Date.now();
+   this.setState({ startTimer: now });
+   console.log(now);
   }
+  showTime(){
+    const {startTimer} = this.state;
+    const now = Date.now();
+    console.log(now);
+    return now - startTimer;
+   }
+   
+   // https://stackoverflow.com/a/42182294/14424360
+   decode(html) {
+     const txt = document.createElement('textarea');
+     txt.innerHTML = html;
+     return txt.value;
+    }
+    // const [counter, setCounter] = React.useState(60);
+    // useEffect(() => {
+    //   counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    // }, [counter]);
+  
 
-  render() {
+    render() {
+    let showAnswers = window.setTimeout(() =>( console.log('Mostra as respostas')) , FIVE_SECONDS);
+    let   initialTimer =  setTimeout(() =>{
+      this.setState({ isTimeout: true })
+      console.log('Trava as questões')
+       showAnswers = window.setTimeout(() =>( console.log('Mostra as respostas')) , FIVE_SECONDS);
+   } ,THIRTY_SECONDS);
     const { resultQuestions = [] } = this.props;
-    const { currentQuestion, goToFeedback } = this.state;
+    const { currentQuestion, goToFeedback ,isTimeout} = this.state;
     if (goToFeedback) { return <Redirect to="/feedback" />; }
     if (!resultQuestions.length) {
       return <div>carregando...</div>;
     }
     return (
       <div>
+        {() => this.startTimer()}
         <Header />
         <div>
           <h1
@@ -61,16 +107,29 @@ class Questions extends Component {
           >
             {this.decode(resultQuestions[currentQuestion].question)}
           </h1>
+          <h2>
+            {this.decode(setInterval(this.showTime()), HALF_SECOND )}
+          </h2>
+          {console.log('Zera os timer')}
+          { window.clearTimeout(initialTimer)}
+          { window.clearTimeout(showAnswers)}
+          {initialTimer }
+          {/* =  setTimeout(() =>{
+      this.setState({ isTimeout: true })
+      console.log('Trava as questões')
+       showAnswers = window.setTimeout(() =>( console.log('Mostra as respostas')) , FIVE_SECONDS);
+   } ,THIRTY_SECONDS)} */}
           <button
             type="button"
             data-testid="correct-answer"
+            disabled={ isTimeout }
           >
             {this.decode(resultQuestions[currentQuestion].correct_answer)}
           </button>
           {resultQuestions[currentQuestion].incorrect_answers.map((e, i) => {
             const datatestid = `wrong-answer-${i}`;
             return (
-              <button key={ i } type="button" data-testid={ datatestid }>{e}</button>
+              <button key={ i } type="button" data-testid={ datatestid } disabled={ isTimeout } >{e}</button>
             );
           })}
         </div>
@@ -79,7 +138,7 @@ class Questions extends Component {
           data-testid="btn-next"
           onClick={ this.handleClick }
         >
-          Próxima Questão
+          Próxima
         </button>
       </div>
     );
