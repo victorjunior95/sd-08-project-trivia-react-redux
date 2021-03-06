7413b7eef7577336698200b156ee3d4a3d1d681a
JRGCast <joao.ralph1@gmail.com>
2021-03-06 13:51:44 -0300

    readequadas variaveis no triviaHeader para pegar informaçoes da chave 'state' e do objeto 'player'

--- src/pages/TriviaHeader.js
+++ src/pages/TriviaHeader.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import md5 from 'crypto-js/md5';
+import { getSpecificObjValue } from '../helpers';
 
 class TriviaHeader extends Component {
   render() {
-    localStorage.setItem('score', 0);
-    const email = localStorage.getItem('gravatarEmail');
+    // localStorage.setItem('score', 0);
+    // const email = localStorage.getItem('gravatarEmail');
+    const email = getSpecificObjValue('state', 'player', 'gravatarEmail');
     const hash = () => md5(email.trim().toLowerCase());
-    const score = localStorage.getItem('score');
-    const playerName = localStorage.getItem('playerName');
+    // const score = localStorage.getItem('score');
+    const score = getSpecificObjValue('state', 'player', 'score');
+    // const playerName = localStorage.getItem('playerName');
+    const playerName = getSpecificObjValue('state', 'player', 'name');
     return (
       <header>
         <img src={ `https://www.gravatar.com/avatar/${hash}?s=20` } data-testid="header-profile-picture" alt="profile" />
