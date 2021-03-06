f8693ec9a2a951c11fafb819f654b228ce1b1203
vivianeflowt <vivianeflowt@gmail.com>
2021-03-06 02:38:28 -0300

    update

--- src/pages/Game/components/GameMatch.jsx
+++ src/pages/Game/components/GameMatch.jsx
@@ -5,19 +5,28 @@ import GameRound from './GameRound';
 
 function GameMatch() {
   const [questions, setQuestions] = useState(null);
-  // const [round, setRound] = useState(null);
+  const [round, setRound] = useState(null);
 
   useEffect(async () => {
     const data = await trivia.getQuestions();
+    setRound(0);
     setQuestions(data);
   }, []);
+
+  // const handleNext = () => {
+  //   //
+  // };
 
   return (
     <div>
       {/* { questions && questions.map((question) => (
         <GameRound key={ question.id } question={ question } />
       ))} */}
-      { questions && <GameRound key={ questions[0].id } question={ questions[0] } /> }
+      { questions
+      && <GameRound
+        key={ questions[round].id }
+        question={ questions[round] }
+      /> }
     </div>
   );
 }
