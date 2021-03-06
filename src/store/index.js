318e5817f4c750373141c39219561d05336764b0
vivianeflowt <vivianeflowt@gmail.com>
2021-03-05 23:32:02 -0300

    update

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import question from '../reducers/question';
+
+const rootReducer = combineReducers({
+  question,
+});
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
