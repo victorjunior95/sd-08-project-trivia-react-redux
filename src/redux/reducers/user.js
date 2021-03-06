296e8aee128a51c9ecd7cd8371d0bd149472274f
Thais Quintela <thais.s.quintela@gmail.com>
2021-03-05 21:27:51 -0300

    correcao lint

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -2,7 +2,11 @@ const INITIAL_STATE = { name: '', emailGravatar: '', token: '' };
 const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case 'USER':
-    return { ...state, name: action.payload.name, emailGravatar: action.payload.emailGravatar };
+    return {
+      ...state,
+      name: action.payload.name,
+      emailGravatar: action.payload.emailGravatar,
+    };
   case 'TOKEN':
     return { ...state, token: action.payload.token };
   default:
