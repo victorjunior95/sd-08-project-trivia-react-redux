153c19453530559ddd9a22f50647c1ee0dfc5fe2
Rosiele David <rosieleserpa@hotmail.com>
2021-03-07 12:15:40 -0300

    refatorando api 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,22 @@
-// const fetch = require('node-fetch');
+const fetch = require('node-fetch');
 
 export async function getToken() {
   const response = await fetch('https://opentdb.com/api_token.php?command=request').then((data) => data.json());
   return response.token;
 }
 
+const newArray = (element) => [...element.incorrect_answers
+  .map((answer) => [answer, 'wrong-answer-']),
+[element.correct_answer, 'correct-answer']];
+
+const shuffleArray = (array) => array.map((a) => ({ sort: Math.random(), value: a }))
+  .sort((a, b) => a.sort - b.sort)
+  .map((a) => a.value);
+  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
 const newResults = (results) => results.map((element) => ({
   ...element,
-  allAnswer: [...element.incorrect_answers
-    .map((answer) => [answer, 'wrong-answer-']),
-  [element.correct_answer, 'correct-answer']]
-    .map((a) => ({ sort: Math.random(), value: a }))
-    .sort((a, b) => a.sort - b.sort)
-    .map((a) => a.value),
-  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  allAnswer: shuffleArray(newArray(element)),
   correctAnswer: element.correct_answer,
   incorrectAnswers: [...element.incorrect_answers],
 }));
@@ -27,5 +30,8 @@ export async function getQuiz() {
   const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`).then((data) => data.json());
   const { results } = response;
   const newArrayObject = newResults(results);
+  console.log(deleteIncorrectAndCorrectAnswers(newArrayObject));
   return deleteIncorrectAndCorrectAnswers(newArrayObject);
 }
+
+// getQuiz();
