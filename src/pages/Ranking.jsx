3580c6af95ac439490edd0c948f93ee1c91e90d9
UmberBn <humberto-bandeira@hotmail.com>
2021-03-07 15:44:54 -0400

    Requisito 18 Concluido

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
-      <title data-testid="ranking-title">Ranking</title>
+      <section>
+        <title data-testid="ranking-title">Ranking</title>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-go-home"
+          >
+            Home
+          </button>
+        </Link>
+      </section>
+
     );
   }
 }
