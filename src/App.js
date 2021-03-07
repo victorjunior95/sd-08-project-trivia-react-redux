b6589733a3e83c4868e714f253736286db23097c
UmberBn <humberto-bandeira@hotmail.com>
2021-03-07 15:32:11 -0400

    Requisito 16 concluido

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Config from './pages/Config';
 import Home from './pages/Home';
 import Game from './pages/Game';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Home } />
       <Route path="/game" component={ Game } />
-      <Route exact path="/config" component={ Config } />
-      <Route exact path="/feedback" component={ Feedback } />
+      <Route path="/config" component={ Config } />
+      <Route path="/feedback" component={ Feedback } />
+      <Route path="/ranking" component={ Ranking } />
     </Switch>
   );
 }
---
+++ src/pages/Ranking.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Ranking extends Component {
+  render() {
+    return (
+      <title data-testid="ranking-title">Ranking</title>
+    );
+  }
+}
+
+export default Ranking;
