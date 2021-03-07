c0736945ec4c667f6816577a9dc278313da2a560
UmberBn <humberto-bandeira@hotmail.com>
2021-03-07 15:15:37 -0400

    Requisitos 14 e 15 concluidos

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../common/components/Header/Header';
 
 class Feedback extends React.Component {
   render() {
     const state = JSON.parse(localStorage.getItem('state'));
-    const { assertions } = state.player;
+    const { assertions, score } = state.player;
     const THREE_ASSERTIONS = 3;
     return (
       <section>
@@ -13,6 +14,16 @@ class Feedback extends React.Component {
           {console.log(assertions)}
           { assertions < THREE_ASSERTIONS ? 'Podia ser melhor...' : 'Mandou bem!' }
         </p>
+        <span data-testid="feedback-total-score">{score}</span>
+        <span data-testid="feedback-total-question">{assertions}</span>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-play-again"
+          >
+            Jogar Novamente
+          </button>
+        </Link>
       </section>
     );
   }
