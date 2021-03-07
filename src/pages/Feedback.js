1394e0d1637943a3cb9737982201aff8aa9d2607
Ana Karine <anakarine.ti@gmail.com>
2021-03-07 03:35:50 -0300

    req. 15

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import Header from '../components/Header';
 
 export default class Feedback extends Component {
@@ -6,6 +7,16 @@ export default class Feedback extends Component {
     return (
       <div data-testid="feedback-text">
         <Header />
+        <NavLink
+          to="/"
+        >
+          <button
+            type="button"
+            data-testid="btn-play-again"
+          >
+            Jogar novamente
+          </button>
+        </NavLink>
       </div>
     );
   }
