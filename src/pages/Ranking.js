34f7e1331de8ae4e809f86e799e18b2edb0ce072
Ana Karine <anakarine.ti@gmail.com>
2021-03-07 03:57:09 -0300

    req. 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,10 +1,24 @@
+// https://stackoverflow.com/questions/51393153/react-routing-redirect-onclick
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
-      <div data-testid="ranking-title">
-        Tela de ranking
+      <div>
+        <div data-testid="ranking-title">
+          Tela de ranking
+        </div>
+        <NavLink
+          to="/"
+        >
+          <button
+            type="button"
+            data-testid="btn-go-home"
+          >
+            Jogar novamente
+          </button>
+        </NavLink>
       </div>
     );
   }
