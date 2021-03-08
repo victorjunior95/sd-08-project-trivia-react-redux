c2d5fc8c2c37a00c3fc7e2965f5b263a66172b68
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-03-08 16:57:30 -0300

    Requisito-14

--- src/pages/Feedback/index.js
+++ src/pages/Feedback/index.js
@@ -5,6 +5,7 @@ class Feedback extends React.Component {
     super(props);
 
     this.loadAssertions = this.loadAssertions.bind(this);
+    this.renderMessage = this.renderMessage.bind(this);
   }
 
   loadAssertions() {
@@ -12,10 +13,14 @@ class Feedback extends React.Component {
     console.log(stateString);
     const stateObject = JSON.parse(stateString);
     console.log(stateObject);
-    const { player: { assertions } } = stateObject;
+    const { player: { assertions, score } } = stateObject;
     console.log(assertions);
+    return { assertions, score };
+  }
+
+  renderMessage() {
     const THREE = 3;
-    if (assertions < THREE) {
+    if (this.loadAssertions().assertions < THREE) {
       return 'Podia ser melhor...';
     }
     return 'Mandou bem!';
@@ -24,8 +29,23 @@ class Feedback extends React.Component {
   render() {
     this.loadAssertions();
     return (
-      <div data-testid="feedback-text">
-        {this.loadAssertions()}
+      <div>
+        <div data-testid="feedback-text">
+          {this.renderMessage()}
+        </div>
+        <div>
+          Placar final:
+        </div>
+        <div data-testid="feedback-total-score">
+          {this.loadAssertions().score}
+        </div>
+        <div>
+          Respostas certas:
+        </div>
+        <div data-testid="feedback-total-question">
+          {this.loadAssertions().assertions}
+        </div>
+        <div />
       </div>
     );
   }
