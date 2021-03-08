3bf290d0cb423b719181b939926e22a85df67026
VitorLimaRios <vitorlimarios999@gmail.com>
2021-03-08 17:33:12 -0300

    [Group-19][Req-18] - Adicionando botao para voltar para tela de login

--- src/pages/Rank.js
+++ src/pages/Rank.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Rank extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loginRedirect: false,
+    };
+  }
+
   render() {
-    return <div>INICIO</div>;
+    const { loginRedirect } = this.state;
+    if (loginRedirect) return <Redirect to="/" />;
+    return (
+      <div>
+        <button type="button" data-testid="btn-go-home">Voltar</button>
+      </div>
+    );
   }
 }
 
