2db1f4bc620af114d2a687eb8edfa3d66a489319
Bruno Martins <bruno.martins217@gmail.com>
2021-03-08 20:02:58 -0300

    :package: Requisito 15

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import { Login, TriviaGame, Settings, Feedback } from './pages';
 
 export default function App() {
   return (
-    <Switch>
-      <Route path="/settings" component={ Settings } />
-      <Route path="/feedback" component={ Feedback } />
-      <Route path="/triviagame" component={ TriviaGame } />
-      <Route exact path="/" component={ Login } />
-    </Switch>
+    <div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/triviagame" component={ TriviaGame } />
+        <Route path="/feedback" component={ Feedback } />
+        <Route path="/settings" component={ Settings } />
+      </Switch>
+    </div>
   );
 }
