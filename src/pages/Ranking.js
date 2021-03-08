9d9e87e6d3406b739074332b0a2107f246df92b5
Aline Debastiani <debastianialine@gmail.com>
2021-03-08 18:09:30 -0300

    cumpre requisito 14

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,14 +1,26 @@
 // https://stackoverflow.com/questions/51393153/react-routing-redirect-onclick
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import gravatarAPI from '../services/gravatarAPI';
 
 class Ranking extends Component {
   render() {
+    const stateString = localStorage.getItem('state');
+    const state = JSON.parse(stateString);
+    const { score, namePlayer, emailPlayer, index } = state.player;
     return (
       <div>
         <div data-testid="ranking-title">
           Tela de ranking
         </div>
+        <img src={ gravatarAPI(emailPlayer) } alt={ namePlayer } />
+        <p data-testid={ `player-name-${index}` }>{ namePlayer }</p>
+        <p
+          data-testid={ `player-score-${index}` }
+        >
+          { score }
+        </p>
         <NavLink
           to="/"
         >
@@ -23,5 +35,13 @@ class Ranking extends Component {
     );
   }
 }
+
+Ranking.propTypes = {
+  player: PropTypes.shape({
+    name: PropTypes.string,
+    email: PropTypes.string,
+    score: PropTypes.number,
+  }).isRequired,
+};
 
 export default Ranking;
