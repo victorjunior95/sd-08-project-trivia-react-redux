b0aa06efc8b61f8214caac3b1ca20d1e66e85d41
VitorLimaRios <vitorlimarios999@gmail.com>
2021-03-08 18:04:37 -0300

    [Group-19][Req-12/16] - Adicionando req 12 ao 16

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,16 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 class Feedback extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loginRedirect: false,
+      rankingRedirect: false,
+    };
+  }
+
+  feedbackMessage(score) {
+    const THREE = 3;
+    if (score >= THREE) return <p data-testid="feedback-text">Mandou bem!</p>;
+    return <p data-testid="feedback-text">Podia ser melhor...</p>;
+  }
+
   render() {
-    const { pic, name, score } = this.props;
-    console.log(this.state);
+    const { pic, name } = this.props;
+    const { loginRedirect, rankingRedirect } = this.state;
+    const { player: { score, assertions } } = JSON.parse(localStorage.getItem('state'));
+    if (loginRedirect) return <Redirect to="/" />;
+    if (rankingRedirect) return <Redirect to="/ranking" />;
     return (
       <>
-        <img alt="Profile" src={ pic } data-testid="header-profile-picture" />
-        <p data-testid="header-player-name">{ name }</p>
-        <p data-testid="header-score">{ score }</p>
+        <header>
+          <img alt="Profile" src={ pic } data-testid="header-profile-picture" />
+          <p data-testid="header-player-name">{ name }</p>
+          <p data-testid="header-score">{ score }</p>
+        </header>
+        { this.feedbackMessage(score) }
+        <p data-testid="feedback-total-score">{ score }</p>
+        <p data-testid="feedback-total-question">{ assertions }</p>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ () => this.setState({ loginRedirect: true }) }
+        >
+          Jogar novamente
+        </button>
+        <button
+          type="button"
+          data-testid="btn-ranking"
+          onClick={ () => this.setState({ rankingRedirect: true }) }
+        >
+          Ver Ranking
+        </button>
       </>
     );
   }
@@ -19,13 +57,11 @@ class Feedback extends React.Component {
 const mapStateToProps = (state) => ({
   pic: state.login.player.gravatarEmail,
   name: state.login.player.name,
-  score: state.game.player.score,
 });
 
 Feedback.propTypes = {
   pic: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  score: PropTypes.string.isRequired,
 };
 
 export default connect(mapStateToProps)(Feedback);
