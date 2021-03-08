bffc2b8e756eb78fce1a93be79ff88f70220ea94
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-03-08 17:29:13 -0300

    Feedback Page

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Feedback extends React.Component {
   render() {
-    return <div data-testid="feedback-text">INICIO</div>;
+    const { pic, name, score } = this.props;
+    console.log(this.state);
+    return (
+      <>
+        <img alt="Profile" src={ pic } data-testid="header-profile-picture" />
+        <p data-testid="header-player-name">{ name }</p>
+        <p data-testid="header-score">{ score }</p>
+      </>
+    );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  pic: state.login.player.gravatarEmail,
+  name: state.login.player.name,
+  score: state.game.player.score,
+});
+
+Feedback.propTypes = {
+  pic: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  score: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
