2b1afb58066806e8bf321620b05afeb371110c96
Mario Pardo <mariio.pardojr@gmail.com>
2021-03-09 16:28:28 -0300

    Requisito 12, criaçao da pagina

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
     return (
-      <h2>Tela de feedback!</h2>
+      <>
+        <h2>Tela de feedback!</h2>
+        <Header />
+      </>
     );
   }
 }
