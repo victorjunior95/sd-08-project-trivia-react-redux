1efdd37658d13f7c693ab72470a07ad19d6560a5
Daniel <marcio.daniel@msn.com>
2021-03-09 18:26:48 -0300

    Ultimo retoque

--- src/pages/ranking/index.js
+++ src/pages/ranking/index.js
@@ -11,15 +11,15 @@ class Ranking extends React.Component {
   render() {
     const ranking = this.getRanking().sort((a, b) => b.score - a.score);
 
-    // const reducedRanking = ranking.reduce((acc, cur) => {
-    //   if (!acc.some((item) => item.picture === cur.picture)) acc.push(cur);
-    //   return acc;
-    // }, []);
+    const reducedRanking = ranking.reduce((acc, cur) => {
+      if (!acc.some((item) => item.picture === cur.picture)) acc.push(cur);
+      return acc;
+    }, []);
 
     return (
       <section>
         <h3 data-testid="ranking-title">Ranking</h3>
-        {ranking.map((player, index) => (
+        {reducedRanking.map((player, index) => (
           <p key={ index }>
             <span data-testid={ `player-name-${index}` }>{player.name}</span>
             <span data-testid={ `player-score-${index}` }>
