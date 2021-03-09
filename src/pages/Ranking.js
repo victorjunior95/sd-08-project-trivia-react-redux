6d87e02c202d425c370e12a845a2f0e380167c90
josebejohann <josebernardojohann@gmail.com>
2021-03-09 08:56:26 -0300

    Creates a button to redirect to the initial page from Ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <main>
+        <h1 data-testid="ranking-title">Ranking</h1>
+
+        <Link
+          to="/"
+          data-testid="btn-go-home"
+        >
+          Voltar ao início
+        </Link>
+      </main>
     );
   }
 }
