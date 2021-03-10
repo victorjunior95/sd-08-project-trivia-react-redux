f43333b4ae576498bcf3f9ef33b9c4ed17717977
LukeCruz <enio.cruz@yahoo.com.br>
2021-03-10 13:01:49 -0300

    14.2

--- src/components/FeedbackMessage.js
+++ src/components/FeedbackMessage.js
@@ -31,21 +31,21 @@ class FeedbackMessage extends React.Component {
     return (
       <>
         { this.message() }
-        <h3>          
+        <div>
           <div data-testid="feedback-total-score">
             {this.messageScore()}
           </div>
           <div data-testid="feedback-total-question">
             {this.message()}
           </div>
-            <button
-            type="button"
-            data-testid="btn-ranking"
-            onClick={ this.handleButtonClickRanking }
-            >
+          <button
+              type="button"
+              data-testid="btn-ranking"
+              onClick={ this.handleButtonClickRanking }
+          >
             Ver Ranking
-        </button>
-       </h3>
+          </button>
+        </div>
       </>
     );
   }
