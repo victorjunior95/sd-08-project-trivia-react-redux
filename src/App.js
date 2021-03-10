427b8d816b34d8edca52316a83c05c4e188a0c70
LukeCruz <enio.cruz@yahoo.com.br>
2021-03-10 11:17:54 -0300

    teste de requisitos no remoto

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Gamescreen from './pages/Gamescreen';
 import Settings from './pages/Settings';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/settings" component={ Settings } />
+        <Route path="/ranking" component={ Ranking } />
         <Route path="/feedback" component={ Feedback } />
         <Route exact path="/gamescreen" component={ Gamescreen } />
       </Switch>
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleButtonClickPlayAgain = this.handleButtonClickPlayAgain.bind(this);
+    this.handleButtonClickRanking = this.handleButtonClickRanking.bind(this);
+  }
+
+  handleButtonClickPlayAgain() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
+  handleButtonClickRanking() {
+    const { history } = this.props;
+    history.push('/ranking');
+  }
+
   render() {
     return (
       <>
         <Header />
         <h2 data-testid="feedback-text">Tela de feedback!</h2>
+        <div data-testid="feedback-total-score">
+            {this.messageScore()}
+        </div>
+        <div data-testid="feedback-total-question">
+          {this.messageAssertions()}
+        </div>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.handleButtonClickPlayAgain }
+        >
+          Jogar novamente
+        </button>
+        <button
+          type="button"
+          data-testid="btn-ranking"
+          onClick={ this.handleButtonClickRanking }
+        >
+          Ver Ranking
+        </button>
       </>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Feedback;
