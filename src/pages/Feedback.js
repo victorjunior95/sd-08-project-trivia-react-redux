04eda758ee307f19151931cfbb56af45f717175a
Antonio Schappo <antonioschappo@gmail.com>
2021-03-10 12:00:03 -0300

    First try req. 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import FeedbackMessage from '../components/FeedbackMessage';
 
 class Feedback extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <h2 data-testid="feedback-text">Tela de feedback!</h2>
+        <FeedbackMessage />
       </>
     );
   }
