1cdb3f5339e350b5e177ec9b6201a877b8972a92
Mario Pardo <mariio.pardojr@gmail.com>
2021-03-11 21:24:03 -0300

    Requisito 17, 18, implementa botao de retorno a pagina de login

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 
 class Ranking extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      redirect: false,
+    };
+
+    this.shouldRedirect = this.shouldRedirect.bind(this);
+  }
+
+  shouldRedirect() {
+    this.setState({
+      redirect: true,
+    });
+  }
+
   render() {
+    const { redirect } = this.state;
     const rankingList = JSON.parse(localStorage.getItem('ranking'));
     const orderedRanking = rankingList.sort((a, b) => b.score - a.score);
+    if (redirect) return <Redirect to="/" />;
     return (
       <>
         <h1 data-testid="ranking-title">Ranking</h1>
@@ -17,6 +36,13 @@ class Ranking extends React.Component {
               </li>))
           }
         </ul>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.shouldRedirect }
+        >
+          Voltar para o início!
+        </button>
       </>
     );
   }
